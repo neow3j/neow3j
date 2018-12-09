92817752d0a65d1273ec2eb15c12cda5aa14561a
Guilherme Sperb Machado <guil@axlabs.com>
2018-12-09 17:07:13 +0100

    Minor change on file headers.

--- utils/src/main/java/io/neow3j/io/BinaryWriter.java
+++ utils/src/main/java/io/neow3j/io/BinaryWriter.java
@@ -2,7 +2,7 @@
  * MIT License
  * <p>
  * Copyright (c) 2018 AlienWorks
- * Copyright (c) 2018 AxLabs (https://axlabs.com)
+ * Copyright (c) 2018 AxLabs GmbH (https://axlabs.com)
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
