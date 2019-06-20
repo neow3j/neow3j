756e81836ef01f6ad42a98cd11196e1cd8937fae
Guilherme Sperb Machado <guil@axlabs.com>
2019-06-21 01:38:50 +0200

    Add debugging messages to the deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,20 @@
 SNAPSHOT_TAG_PATTERN="^(\d+(\.\d+){1,2}(\.\*)?)-SNAPSHOT$"
 RELEASE_TAG_PATTERN="^(\d+(\.\d+){1,2}(\.\*)?)$"
 
+GPG_ENC_FILE="./scripts/neow3j.gpg.enc"
+GPG_FILE="./scripts/neow3j.gpg"
+
+echo "Tag detected: ${TRAVIS_TAG}"
+
 if echo "${TRAVIS_TAG}" | grep -E -q ${SNAPSHOT_TAG_PATTERN}; then
     PARAMS="-Psnapshot"
+    echo "Setting the build as SNAPSHOT."
 fi
 
 if (echo "${TRAVIS_TAG}" | grep -E -q ${SNAPSHOT_TAG_PATTERN}) || (echo "${TRAVIS_TAG}" | grep -E -q ${RELEASE_TAG_PATTERN}); then
+    echo "It's a valid tag pattern."
     if [ "${TRAVIS_PULL_REQUEST}" == 'false' ]; then
-        openssl aes-256-cbc -K $encrypted_f2caa0a2639d_key -iv $encrypted_f2caa0a2639d_iv -in ./scripts/neow3j.gpg.enc -out ./scripts/neow3j.gpg -d
-        ./gradlew releaseAndClose -PnexusUsername=${SONATYPE_USERNAME} -PnexusPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=./scripts/neow3j.gpg ${PARAMS}
+        openssl aes-256-cbc -K $encrypted_f2caa0a2639d_key -iv $encrypted_f2caa0a2639d_iv -in ${GPG_ENC_FILE} -out ${GPG_FILE} -d
+        ./gradlew releaseAndClose -PnexusUsername=${SONATYPE_USERNAME} -PnexusPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=${GPG_FILE} ${PARAMS}
     fi
 fi
