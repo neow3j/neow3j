673efefe8e71b87ceafa930aaee37a20ab9dd2f4
Guilherme Sperb Machado <guil@axlabs.com>
2019-06-21 01:56:02 +0200

    Fix deploy script [3].

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 SNAPSHOT_TAG_PATTERN="^(\d+(\.\d+){1,2}(\.\*)?)-SNAPSHOT$"
 RELEASE_TAG_PATTERN="^(\d+(\.\d+){1,2}(\.\*)?)$"
@@ -8,14 +8,14 @@ GPG_FILE="./scripts/neow3j.gpg"
 
 echo "Tag detected: ${TRAVIS_TAG}"
 
-if echo "${TRAVIS_TAG}" | grep -E -q ${SNAPSHOT_TAG_PATTERN}; then
+if echo "${TRAVIS_TAG}" | grep -P -q ${SNAPSHOT_TAG_PATTERN}; then
     PARAMS="-Psnapshot"
     echo "Setting the build as SNAPSHOT."
 fi
 
-if (echo "${TRAVIS_TAG}" | grep -E -q ${SNAPSHOT_TAG_PATTERN}) || (echo "${TRAVIS_TAG}" | grep -E -q ${RELEASE_TAG_PATTERN}); then
+if (echo "${TRAVIS_TAG}" | grep -P -q ${SNAPSHOT_TAG_PATTERN}) || (echo "${TRAVIS_TAG}" | grep -P -q ${RELEASE_TAG_PATTERN}); then
     echo "It's a valid tag pattern."
-    if [ "${TRAVIS_PULL_REQUEST}" == 'false' ]; then
+    if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
         openssl aes-256-cbc -K $encrypted_f2caa0a2639d_key -iv $encrypted_f2caa0a2639d_iv -in ${GPG_ENC_FILE} -out ${GPG_FILE} -d
         ./gradlew releaseAndClose -PnexusUsername=${SONATYPE_USERNAME} -PnexusPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=${GPG_FILE} ${PARAMS}
     fi
