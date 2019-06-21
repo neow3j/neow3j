e924baca21fce3fc185ca7bb683a07e6fcd84902
Guilherme Sperb Machado <guil@axlabs.com>
2019-06-21 02:49:09 +0200

    Enhancements to the deploy script [2].

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,19 +9,28 @@ GPG_FILE="${TRAVIS_BUILD_DIR}/scripts/neow3j.gpg"
 echo "Tag detected: ${TRAVIS_TAG}"
 echo "Travis build dir: ${TRAVIS_BUILD_DIR}"
 
-if echo "${TRAVIS_TAG}" | grep -P -q ${SNAPSHOT_TAG_PATTERN}; then
+SNAPSHOT=`echo "${TRAVIS_TAG}" | grep -P ${SNAPSHOT_TAG_PATTERN}`
+RELEASE=`echo "${TRAVIS_TAG}" | grep -P ${RELEASE_TAG_PATTERN}`
+
+if [ "${SNAPSHOT}" == "${TRAVIS_TAG}" ]; then
     PARAMS="-Psnapshot"
     echo "Setting the build as SNAPSHOT."
 fi
+if [ "${RELEASE}" == "${TRAVIS_TAG}" ]; then
+    echo "Setting the build as RELEASE."
+fi
 
-if (echo "${TRAVIS_TAG}" | grep -P -q ${SNAPSHOT_TAG_PATTERN}) || (echo "${TRAVIS_TAG}" | grep -P -q ${RELEASE_TAG_PATTERN}); then
+if [ "${SNAPSHOT}" == "${TRAVIS_TAG}" ] || [ "${RELEASE}" == "${TRAVIS_TAG}" ]; then
     echo "It's a valid tag pattern."
     if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+
+        # re-creating the .gpg file
         openssl aes-256-cbc -K $encrypted_f2caa0a2639d_key -iv $encrypted_f2caa0a2639d_iv -in ${GPG_ENC_FILE} -out ${GPG_FILE} -d
+
         # release (uploadArchives)
         ./gradlew release -PnexusUsername=${SONATYPE_USERNAME} -PnexusPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=${GPG_FILE} ${PARAMS}
 
-        if [ $? -eq 0 ]; then
+        if [ $? -eq 0 ] && [ "${RELEASE}" == "${TRAVIS_TAG}" ]; then
             # After releasing (uploadArchives) to nexus,
             # it's a good practice to wait 1 minute or so
             # due to issues with nexus servers.
