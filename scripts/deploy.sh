4f5e26d2907d14991e51b75f1be9b7923282cef3
Guilherme Sperb Machado <guil@axlabs.com>
2019-06-21 02:19:56 +0200

    Instead of releasing and closing in gradle, put the logic to the deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,6 +17,15 @@ if (echo "${TRAVIS_TAG}" | grep -P -q ${SNAPSHOT_TAG_PATTERN}) || (echo "${TRAVI
     echo "It's a valid tag pattern."
     if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
         openssl aes-256-cbc -K $encrypted_f2caa0a2639d_key -iv $encrypted_f2caa0a2639d_iv -in ${GPG_ENC_FILE} -out ${GPG_FILE} -d
-        ./gradlew releaseAndClose -PnexusUsername=${SONATYPE_USERNAME} -PnexusPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=${GPG_FILE} ${PARAMS}
+        # release (uploadArchives)
+        ./gradlew release -PnexusUsername=${SONATYPE_USERNAME} -PnexusPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=${GPG_FILE} ${PARAMS}
+
+        # After releasing (uploadArchives) to nexus,
+        # it's a good practice to wait 1 minute or so
+        # due to issues with nexus servers.
+        sleep 60
+
+        # close and release repo!
+        ./gradlew closeAndReleaseRepository -PnexusUsername=${SONATYPE_USERNAME} -PnexusPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=${GPG_FILE} ${PARAMS}
     fi
 fi
