807c0380fff25f67b1f1c0db40c0bae0510966d8
Guilherme Sperb Machado <guil@axlabs.com>
2020-05-25 10:53:47 +0200

    #106: (WIP) transaction attibutes and cosigner using jackson annotations

--- core/src/main/java/io/neow3j/protocol/core/methods/response/TransactionAttribute.java
+++ core/src/main/java/io/neow3j/protocol/core/methods/response/TransactionAttribute.java
@@ -1,81 +1,42 @@
 package io.neow3j.protocol.core.methods.response;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import io.neow3j.transaction.TransactionAttributeType;
-import io.neow3j.utils.Numeric;
 
-import java.util.Arrays;
-import java.util.Objects;
-
+@JsonTypeInfo(use = Id.NAME, property = "type", include = As.EXISTING_PROPERTY)
+@JsonSubTypes(value = {
+        @JsonSubTypes.Type(value = TransactionCosigner.class, name =
+                TransactionAttributeType.COSIGNER_VALUE)
+})
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TransactionAttribute {
+public abstract class TransactionAttribute {
 
-    @JsonProperty("usage")
-    public TransactionAttributeType usage;
-
-    @JsonProperty("data")
-    private byte[] data;
+    @JsonProperty("type")
+    public TransactionAttributeType type;
 
     public TransactionAttribute() {
     }
 
-    public TransactionAttribute(TransactionAttributeType usage, byte[] data) {
-        this.usage = usage;
-        this.data = data;
+    public TransactionAttribute(TransactionAttributeType type) {
+        this.type = type;
     }
 
-    public TransactionAttribute(TransactionAttributeType usage, String data) {
-        this.usage = usage;
-        this.data = (data != null ? Numeric.hexStringToByteArray(data) : null);
+    @JsonIgnore
+    public TransactionCosigner getAsTransactionCosigner() {
+        if (this instanceof TransactionCosigner) {
+            return (TransactionCosigner) this;
+        }
+        throw new IllegalStateException("This object is not of type " +
+                TransactionAttributeType.COSIGNER_VALUE + " but of " + this.type.jsonValue());
     }
 
-    @JsonGetter
-    public TransactionAttributeType getUsage() {
-        return usage;
-    }
-
-    public byte[] getDataAsBytes() {
-        return this.data;
-    }
-
-    @JsonGetter
-    public String getData() {
-        return this.data != null ? Numeric.toHexString(data) : null;
-    }
-
-    @JsonSetter
-    public void setData(String data) {
-        this.data = Numeric.hexStringToByteArray(data);
-    }
-
-    public void setData(byte[] data) {
-        this.data = data;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof TransactionAttribute)) return false;
-        TransactionAttribute that = (TransactionAttribute) o;
-        return getUsage() == that.getUsage() &&
-                Arrays.equals(getDataAsBytes(), that.getDataAsBytes());
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(getUsage());
-        result = 31 * result + Arrays.hashCode(getDataAsBytes());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "TransactionAttribute{" +
-                "usage=" + usage +
-                ", data=" + (data != null ? Numeric.toHexStringNoPrefix(data) : "null") +
-                '}';
+    public TransactionAttributeType getType() {
+        return type;
     }
 }
