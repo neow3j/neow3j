786e9ea2f75fa6b8b34b9d4bb4a467f5f6f54f73
Michael Bucher <michael@axlabs.com>
2020-07-01 14:21:46 +0200

    #131: Remove satisfied todo for empty content setter

--- core/src/test/java/io/neow3j/protocol/core/methods/response/StackItemTest.java
+++ core/src/test/java/io/neow3j/protocol/core/methods/response/StackItemTest.java
@@ -115,7 +115,6 @@ public class StackItemTest extends ResponseTester {
         IntegerStackItem item = rawItem.asInteger();
         assertEquals(new BigInteger("1124"), item.getValue());
 
-        // TODO: 23.06.20 Michael: set empty content to default value - here to BigInteger.ZERO
         json = ""
                 + " {"
                 + "   \"type\": \"Integer\",\n"
