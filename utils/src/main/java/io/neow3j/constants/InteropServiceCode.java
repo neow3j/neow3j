96697db3f29c612c6c66f5d1bc92a3b5c322e87e
Michael Bucher <michael@axlabs.com>
2020-08-10 11:13:04 +0200

    #168: Adapted Interop Service Codes

--- utils/src/main/java/io/neow3j/constants/InteropServiceCode.java
+++ utils/src/main/java/io/neow3j/constants/InteropServiceCode.java
@@ -25,52 +25,62 @@ public enum InteropServiceCode {
     SYSTEM_RUNTIME_GETEXECUTINGSCRIPTHASH("System.Runtime.GetExecutingScriptHash", 400),
     SYSTEM_RUNTIME_GETCALLINGSCRIPTHASH("System.Runtime.GetCallingScriptHash", 400),
     SYSTEM_RUNTIME_GETENTRYSCRIPTHASH("System.Runtime.GetEntryScriptHash", 400),
-    SYSTEM_RUNTIME_CHECKWITNESS("System.Runtine.CheckWitness", 30000),
+    SYSTEM_RUNTIME_CHECKWITNESS("System.Runtime.CheckWitness", 30_000),
     SYSTEM_RUNTIME_GETINVOCATIONCOUNTER("System.Runtime.GetInvocationCounter", 400),
     SYSTEM_RUNTIME_LOG("System.Runtime.Log", 1_000_000),
     SYSTEM_RUNTIME_NOTIFY("System.Runtime.Notify", 1_000_000),
-    SYSTEM_RUNTIME_GETNOTIFICATIONS("System.Runtime.GetNotifications", null), // dynamic calculation
+    SYSTEM_RUNTIME_GETNOTIFICATIONS("System.Runtime.GetNotifications", 10_000),
     SYSTEM_RUNTIME_GASLEFT("System.Runtime.GasLeft", 400),
 
     SYSTEM_STORAGE_GETCONTEXT("System.Storage.GetContext", 400),
     SYSTEM_STORAGE_GETREADONLYCONTEXT("System.Storage.GetReadOnlyContext", 400),
     SYSTEM_STORAGE_ASREADONLY("System.Storage.AsReadOnly", 400),
-    SYSTEM_STORAGE_GET("System.Storage.Get", 1000000),
-    SYSTEM_STORAGE_FIND("System.Storage.Find", 1000000),
+    SYSTEM_STORAGE_GET("System.Storage.Get", 1_000_000),
+    SYSTEM_STORAGE_FIND("System.Storage.Find", 1_000_000),
     SYSTEM_STORAGE_PUT("System.Storage.Put", null), // dynamic calculation
     SYSTEM_STORAGE_PUTEX("System.Storage.PutEx", null), // dynamic calculation
-    SYSTEM_STORAGE_DELETE("System.Storage.Delete", 100000),
+    SYSTEM_STORAGE_DELETE("System.Storage.Delete", 100_000),
 
-    SYSTEM_BINARY_SERIALIZE("System.Binary.Serialize", 100000),
-    SYSTEM_BINARY_DESERIALIZE("System.Binary.Deserialize", 500000),
+    SYSTEM_BINARY_SERIALIZE("System.Binary.Serialize", 100_000),
+    SYSTEM_BINARY_DESERIALIZE("System.Binary.Deserialize", 500_000),
+    SYSTEM_BINARY_BASE64ENCODE("System.Binary.Base64Encode", 100_000),
+    SYSTEM_BINARY_BASE64DECODE("System.Binary.Base64Decode", 100_000),
 
     SYSTEM_BLOCKCHAIN_GETHEIGHT("System.Blockchain.GetHeight", 400),
-    SYSTEM_BLOCKCHAIN_GETBLOCK("System.Blockchain.GetBlock", 2500000),
-    SYSTEM_BLOCKCHAIN_GETTRANSACTION("System.Blockchain.GetTransaction", 1000000),
-    SYSTEM_BLOCKCHAIN_GETTRANSACTIONHEIGHT("System.Blockchain.GetTransactionHeight", 1000000),
-    SYSTEM_BLOCKCHAIN_GETTRANSACTIONFROMBLOCK("System.Blockchain.GetTransactionFromBlock", 1000000),
-    SYSTEM_BLOCKCHAIN_GETCONTRACT("System.Blockchain.GetContract", 1000000),
+    SYSTEM_BLOCKCHAIN_GETBLOCK("System.Blockchain.GetBlock", 2_500_000),
+    SYSTEM_BLOCKCHAIN_GETTRANSACTION("System.Blockchain.GetTransaction", 1_000_000),
+    SYSTEM_BLOCKCHAIN_GETTRANSACTIONHEIGHT("System.Blockchain.GetTransactionHeight", 1_000_000),
+    SYSTEM_BLOCKCHAIN_GETTRANSACTIONFROMBLOCK("System.Blockchain.GetTransactionFromBlock", 1_000_000),
+    SYSTEM_BLOCKCHAIN_GETCONTRACT("System.Blockchain.GetContract", 1_000_000),
+
+    SYSTEM_CALLBACK_CREATE("System.Callback.Create", 400), // DONE
+    SYSTEM_CALLBACK_CREATEFROMMETHOD("System.Callback.CreateFromMethod", 1_000_000),
+    SYSTEM_CALLBACK_CREATEFROMSYSCALL("System.Callback.CreateFromSyscall", 400),
+    SYSTEM_CALLBACK_INVOKE("System.Callback.Invoke", 1_000_000),
 
     SYSTEM_CONTRACT_CREATE("System.Contract.Create", null), // dynamic calculation
     SYSTEM_CONTRACT_UPDATE("System.Contract.Update", null), // dynamic calculation
-    SYSTEM_CONTRACT_DESTROY("System.Contract.Destroy", 1000000),
-    SYSTEM_CONTRACT_CALL("System.Contract.Call", 1000000),
-    SYSTEM_CONTRACT_CALLEX("System.Contract.CallEx", 1000000),
-    SYSTEM_CONTRACT_ISSTANDARD("System.Contract.IsStandard", 30000),
-    SYSTEM_CONTRACT_GETCALLFLAGS("System.Contract.GetCallFlags", 30000),
-    SYSTEM_CONTRACT_CREATESTANDARDACCOUNT("System.Contract.CreateStandardAccount", 10000),
+    SYSTEM_CONTRACT_DESTROY("System.Contract.Destroy", 1_000_000),
+    SYSTEM_CONTRACT_CALL("System.Contract.Call", 1_000_000),
+    SYSTEM_CONTRACT_CALLEX("System.Contract.CallEx", 1_000_000),
+    SYSTEM_CONTRACT_ISSTANDARD("System.Contract.IsStandard", 30_000),
+    SYSTEM_CONTRACT_GETCALLFLAGS("System.Contract.GetCallFlags", 30_000),
+    SYSTEM_CONTRACT_CREATESTANDARDACCOUNT("System.Contract.CreateStandardAccount", 10_000),
 
     SYSTEM_JSON_SERIALIZE("System.Json.Serialize", 100_000),
     SYSTEM_JSON_DESERIALIZE("System.Json.Deserialize", 500_000),
 
     NEO_CRYPTO_SHA256("Neo.Crypto.SHA256", 1_000_000),
     NEO_CRYPTO_RIPEMD160("Neo.Crypto.RIPEMD160", 1_000_000),
-    NEO_CRYPTO_ECDSA_SECP256R1_VERIFY("Neo.Crypto.ECDsa.Secp256r1.Verify", 1_000_000),
-    NEO_CRYPTO_ECDSA_SECP256K1_VERIFY("Neo.Crypto.ECDsa.Secp256k1.Verify", 1_000_000),
+    NEO_CRYPTO_ECDSA_SECP256R1_VERIFY("Neo.Crypto.VerifyWithECDsaSecp256r1", 1_000_000),
+    NEO_CRYPTO_ECDSA_SECP256K1_VERIFY("Neo.Crypto.VerifyWithECDsaSecp256k1", 1_000_000),
     // The price for check multisig is the price for Secp256r1.Verify times the number of signatures
-    NEO_CRYPTO_ECDSA_SECP256R1_CHECKMULTISIG("Neo.Crypto.ECDsa.Secp256r1.CheckMultiSig", null),
+    NEO_CRYPTO_ECDSA_SECP256R1_CHECKMULTISIG("Neo.Crypto.CheckMultisigWithECDsaSecp256r1", null),
     // The price for check multisig is the price for Secp256k1.Verify times the number of signatures
-    NEO_CRYPTO_ECDSA_SECP256K1_CHECKMULTISIG("Neo.Crypto.ECDsa.Secp256k1.CheckMultiSig", null),
+    NEO_CRYPTO_ECDSA_SECP256K1_CHECKMULTISIG("Neo.Crypto.CheckMultisigWithECDsaSecp256k1", null),
+
+    NEO_NATIVE_DEPLOY("Neo.Native.Deploy", null),
+    NEO_NATIVE_CALL("Neo.Native.Call", null),
 
     // Native tokens.
     NEO_NATIVE_TOKENS_NEO("Neo.Native.Tokens.NEO", null),
