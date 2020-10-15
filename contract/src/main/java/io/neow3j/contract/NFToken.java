772ef549226ff79a642158b8833f96a69b9512d3
Michael Bucher <michael@axlabs.com>
2020-10-15 20:17:01 +0200

    #114: Added NFToken class and according tests for most methods of the NEP-11 standard

---
+++ contract/src/main/java/io/neow3j/contract/NFToken.java
@@ -0,0 +1,261 @@
+package io.neow3j.contract;
+
+import io.neow3j.contract.exceptions.UnexpectedReturnTypeException;
+import io.neow3j.model.types.StackItemType;
+import io.neow3j.protocol.Neow3j;
+import io.neow3j.protocol.core.methods.response.StackItem;
+import io.neow3j.transaction.Signer;
+import io.neow3j.wallet.Wallet;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Represents a NEP-11 Non-Fungible Token contract and provides methods to invoke it.
+ */
+public class NFToken extends Token {
+//    Example implementation for Neo2:
+//    https://gist.github.com/hal0x2328/3237fd9f61132cea6b6dd43899947753
+
+    private static final String TRANSFER = "transfer";
+    private static final String OWNER_OF = "ownerOf";
+    private static final String BALANCE_OF = "balanceOf";
+    private static final String TOKENS_OF = "tokensOf";
+
+    /**
+     * Constructs a new <tt>Nep5Token</tt> representing the token contract with the given script
+     * hash. Uses the given {@link Neow3j} instance for all invocations.
+     *
+     * @param scriptHash the token contract's script hash.
+     * @param neow the {@link Neow3j} instance to use for invocations.
+     */
+    public NFToken(ScriptHash scriptHash, Neow3j neow) {
+        super(scriptHash, neow);
+    }
+
+    /**
+     * Transfers the tokenId with {@code tokenId} to the account {@code to}.
+     *
+     * @param to the receiver of the tokenId.
+     * @param tokenId the script hash of the tokenId.
+     * @return a transaction builder.
+     * @throws IOException if there was a problem fetching information from the Neo node.
+     */
+    public TransactionBuilder transfer(Wallet wallet, ScriptHash to, ScriptHash tokenId) throws IOException {
+        int decimals = getDecimals();
+        if (decimals != 0) {
+            throw new IllegalStateException("This method is only implemented on NF tokens that are " +
+                    "divisible. This tokenId has " + decimals + " decimals.");
+        }
+
+        List<ScriptHash> owners = ownerOf(tokenId);
+        if (owners.size() != 1) {
+            throw new IllegalStateException("The tokenId with id " + tokenId.toString() + " has " +
+                    owners.size() + " owners. To transfer fractions use the method transferFractions.");
+        }
+
+        ScriptHash tokenOwner = owners.get(0);
+        if (!wallet.holdsAccount(tokenOwner)) {
+            throw new IllegalArgumentException("The provided wallet does not contain the account that " +
+                    "owns the tokenId " + tokenId.toString() + ". The address of the owner of this tokenId " +
+                    "is " + tokenOwner.toAddress() + ".");
+        }
+
+        return invokeFunction(TRANSFER,
+                ContractParameter.hash160(to),
+                ContractParameter.hash160(tokenId))
+                .wallet(wallet)
+                .signers(Signer.calledByEntry(tokenOwner));
+    }
+
+    /**
+     * Transfers the tokenId with {@code tokenId} to the account {@code to}.
+     *
+     * @param wallet the wallet that holds the {@code from} account.
+     * @param from the account to send the tokenId fractions from.
+     * @param to the receiver of the tokenId fraction.
+     * @param amount the fraction to be transferred.
+     * @param tokenId the script hash of the tokenId.
+     * @return a transaction builder.
+     * @throws IOException if there was a problem fetching information from the Neo node.
+     */
+    public TransactionBuilder transferFraction(Wallet wallet, ScriptHash from, ScriptHash to,
+            BigDecimal amount, ScriptHash tokenId) throws IOException {
+        List<ScriptHash> owners = ownerOf(tokenId);
+
+        if (owners.stream().noneMatch(from::equals)) {
+            throw new IllegalStateException("The account " + from.toAddress() + " is not an owner " +
+                    "of the token with script hash " + tokenId.toString() + ".");
+        }
+
+        if (!wallet.holdsAccount(from)) {
+            throw new IllegalArgumentException("The provided wallet does not contain the provided from " +
+                    "account.");
+        }
+
+        BigInteger fractions = getAmountAsBigInteger(amount);
+        System.out.println(fractions);
+
+        return invokeFunction(TRANSFER,
+                ContractParameter.hash160(from),
+                ContractParameter.hash160(to),
+                ContractParameter.integer(fractions),
+                ContractParameter.hash160(tokenId))
+                .wallet(wallet)
+                .signers(Signer.calledByEntry(from));
+    }
+
+    /**
+     * Gets the owner(s) of the token with script hash {@code tokenId}.
+     *
+     * @param tokenId the script hash of the tokenId.
+     * @return a transaction builder
+     */
+    // According to the not yet final NEP-11 proposal, this method returns an enumerator that contains all
+    //  the co-owners that own the specified token. However, there no stack item type enumerator is
+    //  implemented yet. Hence, this method expects an array.
+    // TODO: 15.10.20 Michael: Adapt this method as soon as an implementation of the NEP-11 proposal exists.
+    public List<ScriptHash> ownerOf(ScriptHash tokenId) throws IOException {
+        return callFunctionReturningListOfScriptHashes(OWNER_OF,
+                Arrays.asList(ContractParameter.hash160(tokenId)));
+    }
+
+    private List<ScriptHash> callFunctionReturningListOfScriptHashes(String function,
+            List<ContractParameter> params) throws IOException {
+
+        StackItem arrayItem = callInvokeFunction(function, params).getInvocationResult().getStack().get(0);
+        if (!arrayItem.getType().equals(StackItemType.ARRAY)) {
+            throw new UnexpectedReturnTypeException(arrayItem.getType(), StackItemType.ARRAY);
+        }
+        List<ScriptHash> scriptHashes = new ArrayList<>();
+        for (StackItem item : arrayItem.asArray().getValue()) {
+            scriptHashes.add(extractScriptHash(item));
+        }
+        return scriptHashes;
+    }
+
+    private ScriptHash extractScriptHash(StackItem item) {
+        if (!item.getType().equals(StackItemType.BYTE_STRING)) {
+            throw new UnexpectedReturnTypeException(item.getType(),
+                    StackItemType.BYTE_STRING);
+        }
+        try {
+            return ScriptHash.fromAddress(item.asByteString().getAsAddress());
+        } catch (IllegalArgumentException e) {
+            throw new UnexpectedReturnTypeException("Byte array return type did not contain "
+                    + "script hash in expected format.", e);
+        }
+    }
+
+//    /**
+//     * Gets the token balance for the given account script hash.
+//     * <p>
+//     * The token amount is returned in token fractions. E.g., an amount of 1 GAS is returned as
+//     * 1*10^8 GAS fractions.
+//     * <p>
+//     * The balance is not cached locally. Every time this method is called requests are send to the
+//     * neo-node.
+//     *
+//     * @param owner The script hash of the account to fetch the balance for.
+//     * @return the token balance.
+//     * @throws IOException                   if there was a problem fetching information from the
+//     *                                       Neo node.
+//     * @throws UnexpectedReturnTypeException if the contract invocation did not return something
+//     *                                       interpretable as a number.
+//     */
+//    // Returns the total amount of NFTs owned by the specified address
+//    public BigInteger balanceOf(ScriptHash owner) {
+//
+//        // This method is only implemented on contracts with decimals = 0
+//        return new BigInteger("0");
+//    }
+
+    /**
+     * Gets the balance of the given token for the given account script hash.
+     * <p>
+     * The balance is returned in token fractions. E.g., a balance of 5 for a token with 2 decimals
+     * is returned as 5*10^2 token fractions.
+     * <p>
+     * The balance is not cached locally. Every time this method is called requests are send to the
+     * neo-node.
+     *
+     * @param owner the script hash of the account to fetch the balance for.
+     * @param tokenId the token id.
+     * @return the token balance of the given account.
+     * @throws IOException                   if there was a problem fetching information from the
+     *                                       Neo node.
+     * @throws UnexpectedReturnTypeException if the contract invocation did not return something
+     *                                       interpretable as a number.
+     */
+    public BigInteger balanceOf(ScriptHash owner, ScriptHash tokenId) throws IOException {
+        return callFuncReturningInt(BALANCE_OF,
+                ContractParameter.hash160(owner),
+                ContractParameter.hash160(tokenId));
+    }
+
+    /**
+     * Gets all the token script hashes owned by the given account.
+     *
+     * @param owner the account.
+     * @return a list of all tokens that are owned by the given account.
+     */
+    public List<ScriptHash> tokensOf(ScriptHash owner) throws IOException {
+        return callFunctionReturningListOfScriptHashes(TOKENS_OF,
+                Arrays.asList(ContractParameter.hash160(owner)));
+    }
+
+    /**
+     * Gets the properties of the given token.
+     *
+     * @param tokenId the token script hash.
+     * @return the properties of the token.
+     */
+    // the response must conform to the `NEO NFT Metadata JSON Schema`.
+    public String getProperties(ScriptHash tokenId) {
+        return "";
+    }
+}
+
+
+
+// Properties structure:
+//  {
+//      "title": "Asset Metadata",
+//      "type": "object",
+//      "properties": {
+//          "name": {
+//              "type": "string",
+//              "description": "Identifies the asset to which this NFT represents."
+//          },
+//          "description": {
+//              "type": "string",
+//              "description": "Describes the asset to which this NFT represents."
+//          },
+//          "image": {
+//              "type": "string",
+//              "description": "Optional. A URI pointing to a resource with mime type image/* representing the asset to which this NFT represents. Consider making any images at a width between 320 and 1080 pixels and aspect ratio between 1.91:1 and 4:5 inclusive."
+//          },
+//          "tokenURI": {
+//              "type": "string",
+//              "description": "Optional. A distinct URI for a given asset that adheres to RFC 3986"
+//          }
+//      }
+//  }
+
+// Most simple properties object:
+//  {
+//      "name": "Slime 1",
+//      "description": "A slime"
+//  }
+
+// If all parts are included:
+//  {
+//      "name": "Slime 2",
+//      "description": "A slime",
+//      "image": "{some image URI}",
+//      "tokenURI": "{some URI}"
+//  }
---
+++ contract/src/test/java/io/neow3j/contract/NFTokenTest.java
@@ -0,0 +1,200 @@
+package io.neow3j.contract;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static io.neow3j.contract.ContractTestHelper.setUpWireMockForCall;
+import static io.neow3j.contract.ContractTestHelper.setUpWireMockForInvokeFunction;
+import io.neow3j.wallet.Wallet;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import io.neow3j.constants.InteropServiceCode;
+import io.neow3j.crypto.ECKeyPair;
+import io.neow3j.protocol.Neow3j;
+import io.neow3j.protocol.http.HttpService;
+import io.neow3j.utils.Numeric;
+import io.neow3j.wallet.Account;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class NFTokenTest {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(options().dynamicPort());
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    private Account account1;
+    private Account account2;
+    private Account account3;
+//    private static final ScriptHash RECIPIENT_SCRIPT_HASH =
+//            new ScriptHash("969a77db482f74ce27105f760efa139223431394");
+    public static final ScriptHash NF_TOKEN_SCRIPT_HASH = ScriptHash.fromScript(
+            new ScriptBuilder().pushData("NFTestToken").sysCall(InteropServiceCode.NEO_NATIVE_CALL).toArray());
+    private static final ScriptHash TOKEN_ID = new ScriptHash("0368df7d189952e05e4045f53856bcfa595070f3");
+    private static final String TRANSFER = "transfer";
+    NFToken nfTestToken;
+
+    @Before
+    public void setUp() {
+        // Configuring WireMock to use default host and the dynamic port set in WireMockRule.
+        int port = this.wireMockRule.port();
+        WireMock.configureFor(port);
+
+        Neow3j neow = Neow3j.build(new HttpService("http://127.0.0.1:" + port));
+        nfTestToken = new NFToken(NF_TOKEN_SCRIPT_HASH, neow);
+
+        // APiZTA6Ym7EHpLK5PLpSLKn62qeMyCZEER
+        account1 = new Account(ECKeyPair.create(
+                Numeric.hexStringToByteArray(
+                        "1dd37fba80fec4e6a6f13fd708d8dcb3b29def768017052f6c930fa1c5d90bbb")));
+        account2 = new Account(ECKeyPair.create(
+                Numeric.hexStringToByteArray(
+                        "b4b2b579cac270125259f08a5f414e9235817e7637b9a66cfeb3b77d90c8e7f9")));
+        account3 = new Account(ECKeyPair.create(
+                Numeric.hexStringToByteArray(
+                        "3a100280baf46ea7db17bc01b53365891876b4a2db11028dbc1ccb8c782725f8")));
+    }
+
+    @Test
+    public void testTransfer() throws IOException {
+        setUpWireMockForInvokeFunction("decimals", "nft_decimals_0.json");
+        setUpWireMockForInvokeFunction("ownerOf", "nft_ownerof.json");
+        setUpWireMockForInvokeFunction("transfer", "nft_transfer.json");
+
+        byte[] expectedScript = new ScriptBuilder().contractCall(NF_TOKEN_SCRIPT_HASH, TRANSFER,
+                Arrays.asList(
+                        ContractParameter.hash160(account1.getScriptHash()),
+                        ContractParameter.hash160(TOKEN_ID)))
+                .toArray();
+
+        Wallet wallet = Wallet.withAccounts(account1);
+        TransactionBuilder b = nfTestToken.transfer(wallet, account1.getScriptHash(), TOKEN_ID);
+        assertThat(b.getScript(), is(expectedScript));
+    }
+
+    @Test
+    public void testTransfer_Divisible() throws IOException {
+        setUpWireMockForInvokeFunction("decimals", "nft_decimals_5.json");
+
+        exceptionRule.expect(IllegalStateException.class);
+        exceptionRule.expectMessage("method is only implemented on NF tokens that are divisible.");
+        nfTestToken.transfer(Wallet.create(), account1.getScriptHash(), TOKEN_ID);
+    }
+
+    @Test
+    public void testTransfer_MultipleOwners() throws IOException {
+        setUpWireMockForInvokeFunction("decimals", "nft_decimals_0.json");
+        setUpWireMockForInvokeFunction("ownerOf", "nft_ownerof_multiple.json");
+
+        exceptionRule.expect(IllegalStateException.class);
+        exceptionRule.expectMessage("has 2 owners. To transfer fractions use the method " +
+                "transferFractions.");
+        nfTestToken.transfer(Wallet.create(), account1.getScriptHash(), TOKEN_ID);
+    }
+
+    @Test
+    public void testTransfer_WalletDoesNotContainTokenOwner() throws IOException {
+        setUpWireMockForInvokeFunction("decimals", "nft_decimals_0.json");
+        setUpWireMockForInvokeFunction("ownerOf", "nft_ownerof.json");
+
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("The provided wallet does not contain the account");
+        nfTestToken.transfer(Wallet.withAccounts(account2), account1.getScriptHash(), TOKEN_ID);
+    }
+
+    @Test
+    public void testTransferFraction() throws IOException {
+        setUpWireMockForInvokeFunction("decimals", "nft_decimals_5.json");
+        setUpWireMockForInvokeFunction("ownerOf", "nft_ownerof.json");
+        setUpWireMockForInvokeFunction("transfer",
+                "nft_transferfractions.json");
+
+        byte[] expectedScript = new ScriptBuilder().contractCall(NF_TOKEN_SCRIPT_HASH, TRANSFER,
+                Arrays.asList(
+                        ContractParameter.hash160(account1.getScriptHash()),
+                        ContractParameter.hash160(account2.getScriptHash()),
+                        ContractParameter.integer(new BigInteger("20000000")),
+                        ContractParameter.hash160(TOKEN_ID)))
+                .toArray();
+
+        Wallet wallet = Wallet.withAccounts(account1);
+        TransactionBuilder b = nfTestToken.transferFraction(wallet, account1.getScriptHash(),
+                account2.getScriptHash(), new BigDecimal("200"), TOKEN_ID);
+
+        assertThat(b.getScript(), is(expectedScript));
+    }
+
+    @Test
+    public void testTransferFraction_FromIsNotAnOwner() throws IOException {
+        setUpWireMockForInvokeFunction("decimals", "nft_decimals_5.json");
+        setUpWireMockForInvokeFunction("ownerOf", "nft_ownerof_multiple.json");
+
+        exceptionRule.expect(IllegalStateException.class);
+        exceptionRule.expectMessage("is not an owner of the token with script hash");
+        nfTestToken.transferFraction(Wallet.create(), account3.getScriptHash(), account1.getScriptHash(),
+                new BigDecimal("10"), TOKEN_ID);
+    }
+
+    @Test
+    public void testTransferFraction_WalletDoesNotContainFromAccount() throws IOException {
+        setUpWireMockForInvokeFunction("decimals", "nft_decimals_5.json");
+        setUpWireMockForInvokeFunction("ownerOf", "nft_ownerof.json");
+
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("The provided wallet does not contain the provided from account");
+        nfTestToken.transferFraction(Wallet.withAccounts(account2), account1.getScriptHash(),
+                account3.getScriptHash(), new BigDecimal("10"), TOKEN_ID);
+    }
+
+    @Test
+    public void testOwnerOf() throws IOException {
+        setUpWireMockForCall("invokescript", "nft_decimals_0.json");
+        setUpWireMockForInvokeFunction("ownerOf", "nft_ownerof.json");
+        List<ScriptHash> owners = nfTestToken.ownerOf(TOKEN_ID);
+
+        assertThat(owners, hasSize(1));
+        assertThat(owners, contains(account1.getScriptHash()));
+    }
+
+    @Test
+    public void testBalanceOf() throws IOException {
+        setUpWireMockForCall("invokescript", "invokescript_transfer.json");
+        setUpWireMockForInvokeFunction("balanceOf", "nft_balanceof.json");
+        BigInteger balance = nfTestToken.balanceOf(account1.getScriptHash(), TOKEN_ID);
+
+        assertThat(balance, is(new BigInteger("244")));
+    }
+
+    @Test
+    public void testTokensOf() throws IOException {
+        setUpWireMockForCall("invokescript", "invokescript_transfer.json");
+        setUpWireMockForInvokeFunction("tokensOf", "nft_tokensof.json");
+        List<ScriptHash> ownedTokens = nfTestToken.tokensOf(account1.getScriptHash());
+
+        assertThat(ownedTokens, hasSize(2));
+        assertThat(ownedTokens, contains(
+                ScriptHash.fromAddress("AQVFzBueoJ4qFcYtZmFLfoCoSs4HMHZ3jj"),
+                ScriptHash.fromAddress("ALBWw6P15gWiCTDNKg8LxFdHDAYxYnt7dN")));
+    }
+
+    @Test
+    public void testGetProperties() throws IOException {
+//        setUpWireMockForCall("invokescript", "invokescript_transfer.json");
+//        setUpWireMockForInvokeFunction("properties", "nft_properties.json");
+    }
+}
