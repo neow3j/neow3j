189e0e6c9089c111b397601a713d854764512581
Michael Bucher <michael@axlabs.com>
2020-12-23 17:59:19 +0100

    #310: Updated ManagementContract and added according tests

--- contract/src/main/java/io/neow3j/contract/ManagementContract.java
+++ contract/src/main/java/io/neow3j/contract/ManagementContract.java
@@ -8,8 +8,10 @@ import io.neow3j.protocol.Neow3j;
 import io.neow3j.protocol.core.methods.response.ContractManifest;
 import io.neow3j.protocol.core.methods.response.NeoGetContractState.ContractState;
 import io.neow3j.protocol.core.methods.response.StackItem;
+import io.neow3j.utils.Numeric;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.Arrays;
 
 /**
@@ -20,6 +22,8 @@ public class ManagementContract extends SmartContract {
     private static final String NAME = "ManagementContract";
     public static final ScriptHash SCRIPT_HASH = getScriptHashOfNativeContract(NAME);
 
+    private static final String GET_MINIMUM_DEPLOYMENT_FEE = "getMinimumDeploymentFee";
+    private static final String SET_MINIMUM_DEPLOYMENT_FEE = "setMinimumDeploymentFee";
     private static final String GET_CONTRACT = "getContract";
     private static final String DEPLOY = "deploy";
     private static final String UPDATE = "update";
@@ -38,50 +42,47 @@ public class ManagementContract extends SmartContract {
     }
 
     /**
+     * Gets the minimum fee required for deployment.
+     *
+     * @return The minimum required fee for contract deployment.
+     * @throws IOException if there was a problem fetching information from the Neo node.
+     */
+    public BigInteger getMinimumDeploymentFee() throws IOException {
+        return callFuncReturningInt(GET_MINIMUM_DEPLOYMENT_FEE);
+    }
+
+    /**
+     * Creates a transaction script to set the minimum deployment fee and initializes
+     * a {@link TransactionBuilder} based on this script.
+     *
+     * @param minimumFee the minimum deployment fee.
+     * @return A transaction builder.
+     */
+    public TransactionBuilder setMinimumDeploymentFee(BigInteger minimumFee) {
+        return invokeFunction(SET_MINIMUM_DEPLOYMENT_FEE, ContractParameter.integer(minimumFee));
+    }
+
+    /**
      * Returns the state of a smart contract.
      *
      * @param scriptHash The script hash of the smart contract.
      * @return The state of the smart contract.
      * @throws IOException if there was a problem fetching information from the Neo node.
      */
-    public ContractState getContractState(ScriptHash scriptHash) throws IOException {
-        StackItem stackItem = callInvokeFunction(GET_CONTRACT,Arrays.asList(ContractParameter.hash160(scriptHash)))
+    public ContractState getContract(ScriptHash scriptHash) throws IOException {
+        StackItem stackItem = callInvokeFunction(GET_CONTRACT,
+                Arrays.asList(ContractParameter.hash160(scriptHash)))
                 .getInvocationResult().getStack().get(0);
-        if (!stackItem.getType().equals(StackItemType.BYTE_STRING)) {
-            throw new UnexpectedReturnTypeException(stackItem.getType(), StackItemType.BYTE_STRING);
+        if (!stackItem.getType().equals(StackItemType.ARRAY)) {
+            throw new UnexpectedReturnTypeException(stackItem.getType(), StackItemType.ARRAY);
         }
-        return stackItem.asByteString().getAsJson(ContractState.class);
-
-        /*
-         TODO: 14.12.20 Michael: check of what type the return actually is.
-          If of type array, use the following:
-                StackItem stackItem = callInvokeFunction(GET_CONTRACT).getInvocationResult()
-                        .getStack().get(0);
-                if (!stackItem.getType().equals(StackItemType.ARRAY)) {
-                    throw new UnexpectedReturnTypeException(stackItem.getType(), StackItemType.ARRAY);
-                }
-                ArrayStackItem arrayItem = stackItem.asArray();
-                BigInteger id = arrayItem.get(0).asInteger().getValue();
-                BigInteger updateCounter = arrayItem.get(1).asInteger().getValue();
-                ScriptHash hash = new ScriptHash(arrayItem.get(2).asByteString().getAsString());
-                byte[] script = arrayItem.get(3).asByteString().getAsString().getBytes();
-                ContractState state = arrayItem.get(4).asByteString().getAsJson(ContractState.class);
-        */
+        int id = stackItem.asArray().get(0).asInteger().getValue().intValue();
+        int updateCounter = stackItem.asArray().get(1).asInteger().getValue().intValue();
+        String hash = Numeric.reverseHexString(stackItem.asArray().get(2).asByteString().getAsHexString());
+        String script = Numeric.toHexStringNoPrefix(stackItem.asArray().get(3).asByteString().getValue());
+        ContractManifest manifest = stackItem.asArray().get(4).asByteString().getAsJson(ContractManifest.class);
+        return new ContractState(id, updateCounter, hash, script, manifest);
     }
 
-    /**
-     * Creates a transaction script for deploying a smart contract and initializes
-     * a {@link TransactionBuilder} based on this script.
-     *
-     * @param nefFile The NEF file of the new smart contract.
-     * @param manifest The contract manifest of the new smart contract.
-     * @return A transaction builder.
-     */
-    public TransactionBuilder deploy(NefFile nefFile, ContractManifest manifest)
-            throws JsonProcessingException {
-
-        // TODO: 14.12.20 Michael: Check whether parameters are in correct form.
-        return invokeFunction(DEPLOY, ContractParameter.byteArray(nefFile.getScript()),
-                ContractParameter.byteArray(objectMapper.writeValueAsBytes(manifest)));
-    }
+    // TODO: 23.12.20 Michael: add deploy, update and destroy methods
 }
