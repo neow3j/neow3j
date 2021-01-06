cd9ea2b188762a85cb5393163fc9d2cdfc7118ec
Claude Muller <claude.muller@protonmail.com>
2021-01-06 14:36:14 +0100

    Apply minor fixex.

--- compiler/src/main/java/io/neow3j/compiler/Compiler.java
+++ compiler/src/main/java/io/neow3j/compiler/Compiler.java
@@ -24,7 +24,6 @@ import io.neow3j.devpack.events.Event;
 import io.neow3j.model.types.ContractParameterType;
 import io.neow3j.protocol.core.methods.response.ContractManifest;
 import io.neow3j.utils.Numeric;
-import io.neow3j.utils.Version;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
