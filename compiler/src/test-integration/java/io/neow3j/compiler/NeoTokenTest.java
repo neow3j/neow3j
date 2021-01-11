bf70d222f8feaecbd4685a558ad83ccbf7fe2491
Claude Muller <claude.muller@protonmail.com>
2021-01-11 14:49:26 +0100

    Write devpack NeoToken tests.

--- compiler/src/test-integration/java/io/neow3j/compiler/NeoTokenTest.java
+++ compiler/src/test-integration/java/io/neow3j/compiler/NeoTokenTest.java
@@ -1,10 +1,20 @@
 package io.neow3j.compiler;
 
+import static io.neow3j.contract.ContractParameter.hash160;
+import static io.neow3j.contract.ContractParameter.integer;
+import static io.neow3j.contract.ContractParameter.publicKey;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import io.neow3j.devpack.contracts.NeoToken;
+import io.neow3j.devpack.contracts.NeoToken.Candidate;
 import io.neow3j.protocol.core.methods.response.NeoInvokeFunction;
+import io.neow3j.protocol.core.methods.response.StackItem;
+import io.neow3j.transaction.Signer;
+import java.io.IOException;
+import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -16,6 +26,89 @@ public class NeoTokenTest extends ContractTest {
     }
 
     @Test
+    public void unclaimedGas() throws IOException {
+        NeoInvokeFunction response = callInvokeFunction(hash160(defaultAccount.getScriptHash()),
+                integer(1));
+
+        assertThat(response.getInvocationResult().getStack().get(0)
+                .asInteger().getValue().intValue(), is(0));
+    }
+
+    @Test
+    public void registerAndUnregisterCandidate() throws IOException {
+        signWithDefaultAccount();
+        NeoInvokeFunction response = callInvokeFunction(
+                publicKey(defaultAccount.getECKeyPair().getPublicKey().getEncoded(true)));
+
+        List<StackItem> l = response.getInvocationResult().getStack().get(0).asArray().getValue();
+        assertTrue(l.get(0).asBoolean().getValue());
+        assertTrue(l.get(1).asBoolean().getValue());
+    }
+
+    @Test
+    public void registerAndGetCandidates() throws IOException {
+        signWithDefaultAccount();
+        NeoInvokeFunction response = callInvokeFunction(
+                publicKey(defaultAccount.getECKeyPair().getPublicKey().getEncoded(true)));
+
+        List<StackItem> l = response.getInvocationResult().getStack().get(0).asArray().getValue();
+        assertThat(l, hasSize(1));
+        assertThat(l.get(0).asStruct().get(0).asByteString().getValue(),
+                is(defaultAccount.getECKeyPair().getPublicKey().getEncoded(true)));
+    }
+
+    @Test
+    public void getNextBlockValidators() throws IOException {
+        NeoInvokeFunction response = callInvokeFunction();
+
+        List<StackItem> l = response.getInvocationResult().getStack().get(0).asArray().getValue();
+        assertThat(l.get(0).asByteString().getValue(),
+                is(defaultAccount.getECKeyPair().getPublicKey().getEncoded(true)));
+    }
+
+    @Test
+    public void getCommittee() throws IOException {
+        NeoInvokeFunction response = callInvokeFunction();
+
+        List<StackItem> l = response.getInvocationResult().getStack().get(0).asArray().getValue();
+        assertThat(l.get(0).asByteString().getValue(),
+                is(defaultAccount.getECKeyPair().getPublicKey().getEncoded(true)));
+    }
+
+    @Test
+    public void setAndGetGasPerBlock() throws Throwable {
+        signAsCommittee();
+        NeoInvokeFunction res = callInvokeFunction("getGasPerBlock");
+        int gasPerBlock = res.getInvocationResult().getStack().get(0).asInteger().getValue()
+                .intValue();
+        assertThat(gasPerBlock, is(500_000_000));
+
+        invokeFunctionAndAwaitExecution("setGasPerBlock", integer(50_000));
+
+        res = callInvokeFunction("getGasPerBlock");
+        gasPerBlock = res.getInvocationResult().getStack().get(0).asInteger().getValue().intValue();
+        assertThat(gasPerBlock, is(50_000));
+    }
+
+    @Test
+    public void vote() throws Throwable {
+        io.neow3j.contract.NeoToken neoToken = new io.neow3j.contract.NeoToken(neow3j);
+        String txHash = neoToken.registerCandidate(defaultAccount.getECKeyPair().getPublicKey())
+                .wallet(wallet)
+                .signers(Signer.calledByEntry(defaultAccount.getScriptHash()))
+                .sign()
+                .send()
+                .getSendRawTransaction().getHash();
+        waitUntilTransactionIsExecuted(txHash);
+
+        signWithDefaultAccount();
+        NeoInvokeFunction res = callInvokeFunction(hash160(defaultAccount.getScriptHash()),
+                publicKey(defaultAccount.getECKeyPair().getPublicKey().getEncoded(true)));
+
+        assertTrue(res.getInvocationResult().getStack().get(0).asBoolean().getValue());
+    }
+
+    @Test
     public void getHash() throws Throwable {
         NeoInvokeFunction response = callInvokeFunction();
         assertThat(response.getInvocationResult().getStack().get(0).asByteString().getAsHexString(),
@@ -23,6 +116,42 @@ public class NeoTokenTest extends ContractTest {
     }
 
     static class NeoTokenTestContract {
+
+        public static int unclaimedGas(byte[] scriptHash, int blockHeight) {
+            return NeoToken.unclaimedGas(scriptHash, blockHeight);
+        }
+
+        public static boolean[] registerAndUnregisterCandidate(byte[] publicKey) {
+            boolean[] b = new boolean[2];
+            b[0] = NeoToken.registerCandidate(publicKey);
+            b[1] = NeoToken.unregisterCandidate(publicKey);
+            return b;
+        }
+
+        public static Candidate[] registerAndGetCandidates(byte[] publicKey) {
+            NeoToken.registerCandidate(publicKey);
+            return NeoToken.getCandidates();
+        }
+
+        public static String[] getNextBlockValidators() {
+            return NeoToken.getNextBlockValidators();
+        }
+
+        public static String[] getCommittee() {
+            return NeoToken.getCommittee();
+        }
+
+        public static int getGasPerBlock() {
+            return NeoToken.getGasPerBlock();
+        }
+
+        public static boolean setGasPerBlock(int gasPerBlock) {
+            return NeoToken.setGasPerBlock(gasPerBlock);
+        }
+
+        public static boolean vote(byte[] scriptHash, byte[] pubKey) {
+            return NeoToken.vote(scriptHash, pubKey);
+        }
 
         public static byte[] getHash() {
             return NeoToken.getHash();
