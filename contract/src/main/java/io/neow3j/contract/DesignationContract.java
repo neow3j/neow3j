49fb18105384c4e1a14be17a11232c2c35d1bad5
Michael Bucher <michael@axlabs.com>
2021-01-14 14:42:04 +0100

    #265: Updated correct name and restructured methods

--- contract/src/main/java/io/neow3j/contract/DesignationContract.java
+++ contract/src/main/java/io/neow3j/contract/DesignationContract.java
@@ -2,10 +2,11 @@ package io.neow3j.contract;
 
 import io.neow3j.constants.InteropServiceCode;
 import io.neow3j.constants.OpCode;
-import io.neow3j.crypto.ECKeyPair.ECPublicKey;
 import io.neow3j.protocol.Neow3j;
 import io.neow3j.protocol.core.DesignationRole;
+import io.neow3j.protocol.core.methods.response.NeoInvokeFunction;
 import io.neow3j.protocol.core.methods.response.StackItem;
+import io.neow3j.wallet.Account;
 import org.bouncycastle.math.ec.ECPoint;
 
 import java.io.IOException;
@@ -18,7 +19,7 @@ import java.util.List;
  */
 public class DesignationContract extends SmartContract {
 
-    private static final String NAME = "Designation";
+    private static final String NAME = "DesignationContract";
 
     private static final byte[] SCRIPT = new ScriptBuilder()
             .pushData(NAME)
@@ -53,24 +54,44 @@ public class DesignationContract extends SmartContract {
      * @return the designates by the given role and index.
      */
     public ECPoint[] getDesignatedByRole(DesignationRole role, int index) throws IOException {
-        StackItem stackItem = callInvokeFunction(GET_DESIGNATED_BY_ROLE, Arrays.asList(
-                ContractParameter.string(role.jsonValue()), ContractParameter.integer(index)))
-                .getInvocationResult().getStack().get(0);
-        // TODO: 14.12.20 Michael: check return type when preview4 node is available.
+//    params:
+//    - role: Integer
+//    - index: Integer
+//    returnType: ECPoint[]
+        NeoInvokeFunction invoc = callInvokeFunction(GET_DESIGNATED_BY_ROLE,
+                Arrays.asList(
+                        ContractParameter.integer(role.byteValue()),
+                        ContractParameter.integer(index)));
+        StackItem stackItem = invoc.getInvocationResult().getStack().get(0);
+        // TODO: 14.12.20 Michael: check return type and implement it.
         return null;
     }
 
     /**
-     * Creates a transaction script to designate accounts as roles and initializes
-     * a {@link TransactionBuilder} based on this script.
+     * Creates a transaction script to designate nodes as a {@link DesignationRole} and
+     * initializes a {@link TransactionBuilder} based on this script.
      *
-     * @param role The role.
-     * @param publicKeys The accounts to designate the role.
+     * @param role  The role.
+     * @param nodes The nodes to be designated.
      */
-    public TransactionBuilder designateAsRole(DesignationRole role, List<ECPublicKey> publicKeys) {
-        List<ContractParameter> params = new ArrayList<>();
-        params.add(ContractParameter.string(role.jsonValue()));
-        publicKeys.forEach(key -> params.add(ContractParameter.publicKey(key.toArray())));
-        return invokeFunction(DESIGNATE_AS_ROLE, params.toArray(new ContractParameter[0]));
+    public TransactionBuilder designateAsRole(DesignationRole role, List<Account> nodes) {
+//    params:
+//    - role: Integer
+//    - nodes: Array of ECPoints
+//    returnType: Void
+        if (role == null) {
+            throw new IllegalArgumentException("Role cannot be null.");
+        }
+        if (nodes.isEmpty()) {
+            throw new IllegalArgumentException("At least one node is required for designation.");
+        }
+        ContractParameter roleParam = ContractParameter.integer(role.byteValue());
+        List<ContractParameter> nodeParamList = new ArrayList<>();
+        nodes.forEach(account -> nodeParamList.add(
+                ContractParameter.publicKey(
+                        account.getECKeyPair().getPublicKey().getEncoded(true))));
+
+        ContractParameter nodesParam = ContractParameter.array(nodeParamList);
+        return invokeFunction(DESIGNATE_AS_ROLE, roleParam, nodesParam);
     }
 }
