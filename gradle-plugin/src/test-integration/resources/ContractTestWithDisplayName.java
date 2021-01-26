5414fe199722bc5520f4a1b6c79071c8599f93d2
Guilherme Sperb Machado <guil@axlabs.com>
2021-01-26 19:33:05 +0100

    #327: add smart contract for test cases

---
+++ gradle-plugin/src/test-integration/resources/ContractTestWithDisplayName.java
@@ -0,0 +1,9 @@
+package io.neow3j.devpack.gradle;
+
+import io.neow3j.devpack.annotations.DisplayName;
+
+@DisplayName("Contract-Test")
+class ContractTest {
+    public static void test() {
+    }
+}
