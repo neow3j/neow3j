66ad7fae34161498462c83b3db7caf1a0e97d707
Guilherme Sperb Machado <guil@axlabs.com>
2021-01-29 15:19:07 +0100

    #354 and #348: add annotation java docs

--- devpack/src/main/java/io/neow3j/devpack/annotations/Group.java
+++ devpack/src/main/java/io/neow3j/devpack/annotations/Group.java
@@ -5,6 +5,21 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used on contract class level to represent a set of mutually trusted
+ * contracts. A contract will trust and allow any contract in the same group to invoke it, and
+ * the user interface will not give any warnings.
+ * <p>
+ * <ul>
+ * <li>A group is identified by a public key and must be accompanied by
+ * a signature for the contract hash to prove that the contract is indeed
+ * included in the group.</li>
+ * <li>The `pubKey` represents the public key of the group, and `signature` is the
+ * signature of the contract hash encoded in Base64.</li>
+ * </ul>
+ * <p>
+ * Usage of this annotation is not mandatory.
+ */
 @Target(ElementType.TYPE)
 @Repeatable(Groups.class)
 public @interface Group {
--- devpack/src/main/java/io/neow3j/devpack/annotations/Trust.java
+++ devpack/src/main/java/io/neow3j/devpack/annotations/Trust.java
@@ -5,6 +5,19 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used on contract class level to represent contracts trusted to be called.
+ * <p>
+ * <ul>
+ * <li>The value is a contract hash or group public key.</li>
+ * <li>It can also be assigned with a wildcard "*". If it is a wildcard "*", then it means that it
+ * trusts any contract.</li>
+ * <li>If a contract is trusted, the user interface will not give any warnings when called by the
+ * contract.</li>
+ * </ul>
+ * <p>
+ * Usage of this annotation is not mandatory.
+ */
 @Target(ElementType.TYPE)
 @Repeatable(Trusts.class)
 public @interface Trust {
