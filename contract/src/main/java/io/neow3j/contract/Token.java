adff8698f9e9ca242c7ebe253a4ca47f64b990bf
Michael Bucher <michael@axlabs.com>
2021-02-09 10:27:00 +0100

    #383: (minor) code structure adjustment in class Token

--- contract/src/main/java/io/neow3j/contract/Token.java
+++ contract/src/main/java/io/neow3j/contract/Token.java
@@ -8,7 +8,8 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 /**
- * Represents a token wrapper class that contains shared methods for the Nep17 and Nep11 token standards.
+ * Represents a token wrapper class that contains shared methods for
+ * the fungible Nep17 and non-fungible Nep11 token standards.
  */
 public class Token extends SmartContract {
 
@@ -16,8 +17,9 @@ public class Token extends SmartContract {
     private static final String SYMBOL = "symbol";
     private static final String DECIMALS = "decimals";
 
-    // It is expected that token contracts return the total supply in fractions of their token.
-    // Therefore an integer is used here instead of a decimal number.
+    // It is expected that token contracts return the total supply in fractions
+    // of their token. Therefore, an integer is used here instead of a decimal
+    // number.
     private BigInteger totalSupply;
     private Integer decimals;
     private String symbol;
@@ -29,15 +31,18 @@ public class Token extends SmartContract {
     /**
      * Gets the symbol of this token.
      * <p>
-     * The return value is retrieved form the neo-node only once and then cached.
+     * The return value is retrieved form the neo-node only once and then
+     * cached.
      *
      * @return the symbol.
-     * @throws IOException                   if there was a problem fetching information from the
-     *                                       Neo node.
-     * @throws UnexpectedReturnTypeException if the contract invocation did not return something
-     *                                       interpretable as a string.
+     * @throws IOException if there was a problem fetching information from the
+     *                     Neo node.
+     * @throws UnexpectedReturnTypeException if the contract invocation did not
+     *                                       return something interpretable as
+     *                                       a string.
      */
-    public String getSymbol() throws IOException, UnexpectedReturnTypeException {
+    public String getSymbol() throws IOException,
+            UnexpectedReturnTypeException {
         if (symbol == null) {
             symbol = callFuncReturningString(SYMBOL);
         }
@@ -47,15 +52,18 @@ public class Token extends SmartContract {
     /**
      * Gets the total supply of this token in fractions.
      * <p>
-     * The return value is retrieved form the neo-node only once and then cached.
+     * The return value is retrieved form the neo-node only once and then
+     * cached.
      *
      * @return the total supply.
-     * @throws IOException                   if there was a problem fetching information from the
-     *                                       Neo node.
-     * @throws UnexpectedReturnTypeException if the contract invocation did not return something
-     *                                       interpretable as a number.
+     * @throws IOException if there was a problem fetching information from the
+     *                     Neo node.
+     * @throws UnexpectedReturnTypeException if the contract invocation did not
+     *                                       return something interpretable as
+     *                                       a number.
      */
-    public BigInteger getTotalSupply() throws IOException, UnexpectedReturnTypeException {
+    public BigInteger getTotalSupply() throws IOException,
+            UnexpectedReturnTypeException {
         if (totalSupply == null) {
             totalSupply = callFuncReturningInt(TOTAL_SUPPLY);
         }
@@ -63,15 +71,18 @@ public class Token extends SmartContract {
     }
 
     /**
-     * Gets the number of fractions that one unit of this token can be divided into.
+     * Gets the number of fractions that one unit of this token can be divided
+     * into.
      * <p>
-     * The return value is retrieved form the neo-node only once and then cached.
+     * The return value is retrieved form the neo-node only once and then
+     * cached.
      *
      * @return the the number of fractions.
-     * @throws IOException                   if there was a problem fetching information from the
-     *                                       Neo node.
-     * @throws UnexpectedReturnTypeException if the contract invocation did not return something
-     *                                       interpretable as a number.
+     * @throws IOException if there was a problem fetching information from the
+     *                     Neo node.
+     * @throws UnexpectedReturnTypeException if the contract invocation did not
+     *                                       return something interpretable as
+     *                                       a number.
      */
     public int getDecimals() throws IOException, UnexpectedReturnTypeException {
         if (decimals == null) {
@@ -85,21 +96,26 @@ public class Token extends SmartContract {
      *
      * @param amount the amount.
      * @return the amount in fractions.
-     * @throws IOException if there was a problem fetching information from the Neo node.
+     * @throws IOException if there was a problem fetching information from the
+     *                     Neo node.
      */
-    protected BigInteger getAmountAsBigInteger(BigDecimal amount) throws IOException {
+    protected BigInteger getAmountAsBigInteger(BigDecimal amount)
+            throws IOException {
         BigDecimal factor = BigDecimal.TEN.pow(getDecimals());
         return amount.multiply(factor).toBigInteger();
     }
 
     /**
-     * Gets the {@code amount} of the token as a decimal number (not token fractions).
+     * Gets the {@code amount} of the token as a decimal number (not token
+     * fractions).
      *
      * @param amount the amount.
      * @return the amount as a decimal number (not token fractions).
-     * @throws IOException if there was a problem fetching information from the Neo node.
+     * @throws IOException if there was a problem fetching information from the
+     *                     Neo node.
      */
-    protected BigDecimal getAmountAsBigDecimal(BigInteger amount) throws IOException {
+    protected BigDecimal getAmountAsBigDecimal(BigInteger amount)
+            throws IOException {
         BigDecimal a = new BigDecimal(amount);
         BigDecimal divisor = BigDecimal.TEN.pow(getDecimals());
         return a.divide(divisor);
