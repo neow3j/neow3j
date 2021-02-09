196f04c81a82ecd54d3200bb023ad2fa5db124c4
Michael Bucher <michael@axlabs.com>
2021-02-09 15:28:16 +0100

    #383: (minor) Restructured NeoToken code lines

--- contract/src/main/java/io/neow3j/contract/NeoToken.java
+++ contract/src/main/java/io/neow3j/contract/NeoToken.java
@@ -9,7 +9,6 @@ import io.neow3j.crypto.ECKeyPair.ECPublicKey;
 import io.neow3j.model.types.StackItemType;
 import io.neow3j.protocol.Neow3j;
 import io.neow3j.protocol.core.methods.response.StackItem;
-import io.neow3j.utils.Numeric;
 import io.neow3j.wallet.Account;
 import java.io.IOException;
 import java.math.BigInteger;
@@ -19,17 +18,20 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Represents the NeoToken native contract and provides methods to invoke all its functions.
+ * Represents the NeoToken native contract and provides methods to invoke
+ * its functions.
  */
 public class NeoToken extends Nep17Token {
 
     public final static String NAME = "NeoToken";
     public final static long NEF_CHECKSUM = 3921333105L;
-    public static final ScriptHash SCRIPT_HASH = getScriptHashOfNativeContract(NEF_CHECKSUM, NAME);
+    public static final ScriptHash SCRIPT_HASH =
+            getScriptHashOfNativeContract(NEF_CHECKSUM, NAME);
 
     public final static int DECIMALS = 0;
     public final static String SYMBOL = "NEO";
-    public final static BigInteger TOTAL_SUPPLY = new BigInteger("100000000");
+    public final static BigInteger TOTAL_SUPPLY =
+            new BigInteger("100000000");
 
     public static final String UNCLAIMED_GAS = "unclaimedGas";
     public static final String REGISTER_CANDIDATE = "registerCandidate";
@@ -37,13 +39,14 @@ public class NeoToken extends Nep17Token {
     public static final String VOTE = "vote";
     public static final String GET_CANDIDATES = "getCandidates";
     public static final String GET_COMMITTEE = "getCommittee";
-    public static final String GET_NEXT_BLOCK_VALIDATORS = "getNextBlockValidators";
+    public static final String GET_NEXT_BLOCK_VALIDATORS =
+            "getNextBlockValidators";
     public static final String SET_GAS_PER_BLOCK = "setGasPerBlock";
     public static final String GET_GAS_PER_BLOCK = "getGasPerBlock";
 
     /**
-     * Constructs a new {@code NeoToken} that uses the given {@link Neow3j} instance for
-     * invocations.
+     * Constructs a new {@code NeoToken} that uses the given {@link Neow3j}
+     * instance for invocations.
      *
      * @param neow The {@link Neow3j} instance to use for invocations.
      */
@@ -52,7 +55,8 @@ public class NeoToken extends Nep17Token {
     }
 
     /**
-     * Returns the name of the NeoToken contract. Doesn't require a call to the neo-node.
+     * Returns the name of the NeoToken contract. Doesn't require a call to the
+     * Neo node.
      *
      * @return the name.
      */
@@ -62,7 +66,8 @@ public class NeoToken extends Nep17Token {
     }
 
     /**
-     * Returns the symbol of the NeoToken contract. Doesn't require a call to the neo-node.
+     * Returns the symbol of the NeoToken contract. Doesn't require a call to
+     * the Neo node.
      *
      * @return the symbol.
      */
@@ -72,7 +77,8 @@ public class NeoToken extends Nep17Token {
     }
 
     /**
-     * Returns the total supply of the NeoToken contract. Doesn't require a call to the neo-node.
+     * Returns the total supply of the NeoToken contract. Doesn't require a
+     * call to the Neo node.
      *
      * @return the total supply.
      */
@@ -82,7 +88,8 @@ public class NeoToken extends Nep17Token {
     }
 
     /**
-     * Returns the number of decimals of the NEO token. Doesn't require a call to the neo-node.
+     * Returns the number of decimals of the NEO token. Doesn't require a call
+     * to the Neo node.
      *
      * @return the number of decimals.
      */
@@ -92,37 +99,44 @@ public class NeoToken extends Nep17Token {
     }
 
     /**
-     * Gets the amount of unclaimed GAS at the given height for the given account.
+     * Gets the amount of unclaimed GAS at the given height for the given
+     * account.
      *
-     * @param account     the account.
+     * @param account the account.
      * @param blockHeight the block height.
      * @return the amount of unclaimed GAS
-     * @throws IOException if there was a problem fetching information from the Neo node.
+     * @throws IOException if there was a problem fetching information from the
+     *                     Neo node.
      */
-    public BigInteger unclaimedGas(Account account, long blockHeight) throws IOException {
+    public BigInteger unclaimedGas(Account account, long blockHeight)
+            throws IOException {
         return unclaimedGas(account.getScriptHash(), blockHeight);
     }
 
     /**
-     * Gets the amount of unclaimed GAS at the given height for the given account.
+     * Gets the amount of unclaimed GAS at the given height for the given
+     * account.
      *
-     * @param scriptHash  the account's script hash.
+     * @param scriptHash the account's script hash.
      * @param blockHeight the block height.
      * @return the amount of unclaimed GAS
-     * @throws IOException if there was a problem fetching information from the Neo node.
+     * @throws IOException if there was a problem fetching information from the
+     *                     Neo node.
      */
-    public BigInteger unclaimedGas(ScriptHash scriptHash, long blockHeight) throws IOException {
+    public BigInteger unclaimedGas(ScriptHash scriptHash, long blockHeight)
+            throws IOException {
         ContractParameter accParam = hash160(scriptHash);
-        ContractParameter heightParam = integer(BigInteger.valueOf(blockHeight));
+        ContractParameter heightParam =
+                integer(BigInteger.valueOf(blockHeight));
         return callFuncReturningInt(UNCLAIMED_GAS, accParam, heightParam);
     }
 
     /**
-     * Creates a {@code TransactionBuilder} for registering a committee candidate with the given
-     * public key.
+     * Creates a {@code TransactionBuilder} for registering a committee
+     * candidate with the given public key.
      * <p>
-     * Note, that the transaction has to be signed with the account corresponding to the public
-     * key.
+     * Note, that the transaction has to be signed with the account
+     * corresponding to the public key.
      *
      * @param candidateKey The public key to register as a candidate.
      * @return A transaction builder.
@@ -130,12 +144,12 @@ public class NeoToken extends Nep17Token {
     public TransactionBuilder registerCandidate(ECPublicKey candidateKey) {
 
         return invokeFunction(REGISTER_CANDIDATE,
-                ContractParameter.publicKey(candidateKey.getEncoded(true)));
+                publicKey(candidateKey.getEncoded(true)));
     }
 
     /**
-     * Creates a transaction script for registering a validator candidate and initializes a {@link
-     * TransactionBuilder} based on this script.
+     * Creates a transaction script for registering a validator candidate and
+     * initializes a {@link TransactionBuilder} based on this script.
      *
      * @param candidateKey The public key to register as a candidate.
      * @return A transaction builder.
@@ -143,46 +157,53 @@ public class NeoToken extends Nep17Token {
     public TransactionBuilder unregisterCandidate(ECPublicKey candidateKey) {
 
         return invokeFunction(UNREGISTER_CANDIDATE,
-                ContractParameter.publicKey(candidateKey.getEncoded(true)));
+                publicKey(candidateKey.getEncoded(true)));
     }
 
     /**
      * Gets the public keys of the current committee members.
      *
      * @return the committee members' public keys.
-     * @throws IOException                   if there was a problem fetching information from the
-     *                                       Neo node.
-     * @throws UnexpectedReturnTypeException If the return type is not an array or the returned
-     *                                       array's elements are not public keys.
+     * @throws IOException if there was a problem fetching information from the
+     *                     Neo node.
+     * @throws UnexpectedReturnTypeException if the return type is not an array
+     *                                       or the returned array's elements
+     *                                       are not public keys.
      */
     public List<ECPublicKey> getCommittee() throws IOException {
         return callFunctionReturningListOfPublicKeys(GET_COMMITTEE);
     }
 
     /**
-     * Gets the public keys of currently registered validator candidates and their NEO balances.
+     * Gets the public keys of currently registered validator candidates and
+     * their NEO balances.
      *
      * @return the candidate public keys and their NEO balances.
-     * @throws IOException                   if there was a problem fetching information from the
-     *                                       Neo node.
-     * @throws UnexpectedReturnTypeException If the return type is not an array or the array
-     *                                       elements are not public keys and node counts.
+     * @throws IOException if there was a problem fetching information from the
+     *                     Neo node.
+     * @throws UnexpectedReturnTypeException if the return type is not an array
+     *                                       or the array elements are not
+     *                                       public keys and node counts.
      */
     public Map<ECPublicKey, Integer> getCandidates() throws IOException {
-        StackItem arrayItem = callInvokeFunction(GET_CANDIDATES).getInvocationResult().getStack()
-                .get(0);
+        StackItem arrayItem = callInvokeFunction(GET_CANDIDATES)
+                .getInvocationResult().getStack().get(0);
         if (!arrayItem.getType().equals(StackItemType.ARRAY)) {
-            throw new UnexpectedReturnTypeException(arrayItem.getType(), StackItemType.ARRAY);
+            throw new UnexpectedReturnTypeException(arrayItem.getType(),
+                    StackItemType.ARRAY);
         }
         Map<ECPublicKey, Integer> validators = new HashMap<>();
         for (StackItem valItem : arrayItem.asArray().getValue()) {
             if (!valItem.getType().equals(StackItemType.STRUCT)) {
-                throw new UnexpectedReturnTypeException(valItem.getType(), StackItemType.STRUCT);
+                throw new UnexpectedReturnTypeException(valItem.getType(),
+                        StackItemType.STRUCT);
             }
-            ECPublicKey key = extractPublicKey(valItem.asStruct().getValue().get(0));
+            ECPublicKey key = extractPublicKey(valItem
+                    .asStruct().getValue().get(0));
             StackItem nrItem = valItem.asStruct().getValue().get(1);
             if (!nrItem.getType().equals(StackItemType.INTEGER)) {
-                throw new UnexpectedReturnTypeException(nrItem.getType(), StackItemType.INTEGER);
+                throw new UnexpectedReturnTypeException(nrItem.getType(),
+                        StackItemType.INTEGER);
             }
             validators.put(key, nrItem.asInteger().getValue().intValue());
         }
@@ -193,21 +214,24 @@ public class NeoToken extends Nep17Token {
      * Gets the public keys of the next block's validators.
      *
      * @return the validators' public keys.
-     * @throws IOException                   if there was a problem fetching information from the
-     *                                       Neo node.
-     * @throws UnexpectedReturnTypeException If the return type is not an array or the returned
-     *                                       array's elements are not public keys.
+     * @throws IOException if there was a problem fetching information from the
+     *                     Neo node.
+     * @throws UnexpectedReturnTypeException if the return type is not an array
+     *                                       or the returned array's elements
+     *                                       are not public keys.
      */
     public List<ECPublicKey> getNextBlockValidators() throws IOException {
         return callFunctionReturningListOfPublicKeys(GET_NEXT_BLOCK_VALIDATORS);
     }
 
-    private List<ECPublicKey> callFunctionReturningListOfPublicKeys(String function)
-            throws IOException {
+    private List<ECPublicKey> callFunctionReturningListOfPublicKeys
+            (String function) throws IOException {
 
-        StackItem arrayItem = callInvokeFunction(function).getInvocationResult().getStack().get(0);
+        StackItem arrayItem = callInvokeFunction(function)
+                .getInvocationResult().getStack().get(0);
         if (!arrayItem.getType().equals(StackItemType.ARRAY)) {
-            throw new UnexpectedReturnTypeException(arrayItem.getType(), StackItemType.ARRAY);
+            throw new UnexpectedReturnTypeException(arrayItem.getType(),
+                    StackItemType.ARRAY);
         }
         List<ECPublicKey> valKeys = new ArrayList<>();
         for (StackItem keyItem : arrayItem.asArray().getValue()) {
@@ -224,8 +248,8 @@ public class NeoToken extends Nep17Token {
         try {
             return new ECPublicKey(keyItem.asByteString().getValue());
         } catch (IllegalArgumentException e) {
-            throw new UnexpectedReturnTypeException("Byte array return type did not contain "
-                    + "public key in expected format.", e);
+            throw new UnexpectedReturnTypeException("Byte array return type" +
+                    " did not contain public key in expected format.", e);
         }
     }
 
@@ -270,7 +294,8 @@ public class NeoToken extends Nep17Token {
      * GAS cost of all transactions in a block.
      *
      * @return the max GAS amount per block.
-     * @throws IOException if there was a problem fetching information from the Neo node.
+     * @throws IOException if there was a problem fetching information from the
+     *                     Neo node.
      */
     public BigInteger getGasPerBlock() throws IOException {
         return callFuncReturningInt(GET_GAS_PER_BLOCK);
@@ -279,7 +304,8 @@ public class NeoToken extends Nep17Token {
     /**
      * Creates a transaction build for setting the GAS amount per block.
      * <p>
-     * This contract invocation can only be successful if it is signed by the network committee.
+     * This contract invocation can only be successful if it is signed by the
+     * network committee.
      *
      * @param gasPerBlock The desired maximum amount of GAS in one block.
      * @return the transaction builder.
@@ -287,5 +313,4 @@ public class NeoToken extends Nep17Token {
     public TransactionBuilder setGasPerBlock(int gasPerBlock) {
         return invokeFunction(SET_GAS_PER_BLOCK, integer(gasPerBlock));
     }
-
 }
