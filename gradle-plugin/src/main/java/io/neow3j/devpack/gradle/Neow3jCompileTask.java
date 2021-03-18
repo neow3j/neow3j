a4fb7a8fa7effba13f14d06c854986dd9df9dd4f
Guilherme Sperb Machado <guil@axlabs.com>
2021-03-18 01:47:33 +0100

    #472: update to gradle 6.8.3 and completely refactor the gradle-plugin module

--- gradle-plugin/src/main/java/io/neow3j/devpack/gradle/Neow3jCompileTask.java
+++ gradle-plugin/src/main/java/io/neow3j/devpack/gradle/Neow3jCompileTask.java
@@ -1,45 +1,37 @@
 package io.neow3j.devpack.gradle;
 
-import java.io.File;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import org.gradle.api.Action;
 import org.gradle.api.DefaultTask;
-import org.gradle.api.plugins.JavaLibraryPlugin;
-import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.TaskAction;
 
-@SuppressWarnings("unchecked")
-public class Neow3jCompileTask extends DefaultTask {
+abstract public class Neow3jCompileTask extends DefaultTask {
 
     public static final String NEOW3J_COMPILE_TASK_NAME = "neow3jCompile";
     public static final String NEOW3J_COMPILER_OPTIONS_NAME = "neow3jCompiler";
     public static final String NEOW3J_DEFAULT_OUTPUT_DIR = "neow3j";
 
-    private Neow3jPluginOptions options;
-    private File projectBuildDir;
-    private Path compilerDefaultOutputDir;
+    @Input
+    abstract public Property<String> getClassName();
 
-    public Neow3jCompileTask() {
-        this.options = getProject().getExtensions()
-                .create(NEOW3J_COMPILER_OPTIONS_NAME, Neow3jPluginOptions.class);
-        this.projectBuildDir = this.getProject().getBuildDir();
-        this.compilerDefaultOutputDir = Paths.get(projectBuildDir.getAbsolutePath(), NEOW3J_DEFAULT_OUTPUT_DIR);
-        this.getProject().getPluginManager().apply(JavaLibraryPlugin.class);
-        this.dependsOn(JavaPlugin.COMPILE_JAVA_TASK_NAME);
-        Action action = new Neow3jCompileAction();
-        this.doLast(action);
-    }
+    @Input
+    @Optional
+    abstract public Property<Boolean> getDebug();
 
-    public Neow3jPluginOptions getOptions() {
-        return this.options;
-    }
+    @Input
+    @Optional
+    abstract public Property<Path> getOutputDir();
 
-    public File getProjectBuildDir() {
-        return projectBuildDir;
-    }
+    @Input
+    @Optional
+    abstract public Property<Path> getProjectBuildDir();
 
-    public Path getCompilerDefaultOutputDir() {
-        return this.compilerDefaultOutputDir;
+    @TaskAction
+    public void execute() {
+        Neow3jCompileAction action = new Neow3jCompileAction();
+        action.execute(this);
     }
 
 }
