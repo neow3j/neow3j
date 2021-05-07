ee54a4b3f520cb704e7f6cda6cf03355f75bee90
Michael Bucher <michael@axlabs.com>
2021-05-07 12:15:30 +0200

    #537: Added toJson method for NFTokenState

--- core/src/main/java/io/neow3j/protocol/core/methods/response/NFTokenState.java
+++ core/src/main/java/io/neow3j/protocol/core/methods/response/NFTokenState.java
@@ -2,6 +2,8 @@ package io.neow3j.protocol.core.methods.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.neow3j.protocol.ObjectMapperFactory;
 
 import java.util.Objects;
 
@@ -42,6 +44,10 @@ public class NFTokenState {
     @Override
     public int hashCode() {
         return Objects.hash(getName());
+    }
+
+    public String toJson() throws JsonProcessingException {
+        return ObjectMapperFactory.getObjectMapper().writeValueAsString(this);
     }
 
     @Override
