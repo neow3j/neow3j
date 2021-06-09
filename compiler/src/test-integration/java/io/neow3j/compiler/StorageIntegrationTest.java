5db106a5f8392d3699f5c296397c4694d1fcbca2
Michael Bucher <michael@axlabs.com>
2021-06-09 18:24:11 +0200

    #513: Add Storage getters that include conversion

--- compiler/src/test-integration/java/io/neow3j/compiler/StorageIntegrationTest.java
+++ compiler/src/test-integration/java/io/neow3j/compiler/StorageIntegrationTest.java
@@ -19,6 +19,7 @@ import org.junit.Test;
 import org.junit.rules.TestName;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 
@@ -43,42 +44,125 @@ public class StorageIntegrationTest {
     public static ContractTestRule ct = new ContractTestRule(
             StorageIntegrationTest.StorageIntegrationTestContract.class.getName());
 
+    private static final String KEY1_HEX = "01";
     private static final String DATA1 = "0001020304";
-    private static final String KEY1 = "01";
 
+    private static final String KEY2_STRING = "hello";
     private static final String DATA2 = "world";
-    private static final String KEY2 = "hello";
+
+    private static final String KEY3_HEX = "04";
+    private static final BigInteger INTEGER3 = BigInteger.valueOf(42);
+
+    private static final String KEY4_STRING = "neow3j";
+    private static final BigInteger INTEGER4 = BigInteger.valueOf(13);
 
     @BeforeClass
     public static void setUp() throws Throwable {
-        ContractParameter key = byteArray(KEY1);
+        ContractParameter key = byteArray(KEY1_HEX);
         ContractParameter data = byteArray(DATA1);
         ct.invokeFunctionAndAwaitExecution("storeData", key, data);
 
-        key = byteArrayFromString(KEY2);
+        key = byteArrayFromString(KEY2_STRING);
         data = byteArrayFromString(DATA2);
         ct.invokeFunctionAndAwaitExecution("storeData", key, data);
+
+        key = byteArray(KEY3_HEX);
+        data = integer(INTEGER3);
+        ct.invokeFunctionAndAwaitExecution("storeInteger", key, data);
+
+        key = string(KEY4_STRING);
+        data = integer(INTEGER4);
+        ct.invokeFunctionAndAwaitExecution("storeInteger", key, data);
     }
 
     @Test
     public void getByByteArrayKey() throws IOException {
-        ContractParameter key = byteArray("01");
+        ContractParameter key = byteArray(KEY1_HEX);
         InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
         assertThat(res.getStack().get(0).getHexString(), is(DATA1));
     }
 
     @Test
     public void getByByteStringKey() throws IOException {
-        ContractParameter key = byteArray("01");
+        ContractParameter key = byteArray(KEY1_HEX);
         InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
         assertThat(res.getStack().get(0).getHexString(), is(DATA1));
     }
 
     @Test
     public void getByStringKey() throws IOException {
-        ContractParameter key = string("hello");
+        ContractParameter key = string(KEY2_STRING);
         InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
         assertThat(res.getStack().get(0).getString(), is(DATA2));
+    }
+
+    // getByteArray
+
+    @Test
+    public void getByteArrayByByteArrayKey() throws IOException {
+        ContractParameter key = byteArray(KEY1_HEX);
+        InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
+        assertThat(res.getStack().get(0).getHexString(), is(DATA1));
+    }
+
+    @Test
+    public void getByteArrayByByteStringKey() throws IOException {
+        ContractParameter key = byteArray(KEY1_HEX);
+        InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
+        assertThat(res.getStack().get(0).getHexString(), is(DATA1));
+    }
+
+    @Test
+    public void getByteArrayByStringKey() throws IOException {
+        ContractParameter key = string(KEY2_STRING);
+        InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
+        assertThat(res.getStack().get(0).getString(), is(DATA2));
+    }
+
+    // getString
+
+    @Test
+    public void getStringByByteArrayKey() throws IOException {
+        ContractParameter key = byteArray(KEY1_HEX);
+        InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
+        assertThat(res.getStack().get(0).getHexString(), is(DATA1));
+    }
+
+    @Test
+    public void getStringByByteStringKey() throws IOException {
+        ContractParameter key = byteArray(KEY1_HEX);
+        InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
+        assertThat(res.getStack().get(0).getHexString(), is(DATA1));
+    }
+
+    @Test
+    public void getStringByStringKey() throws IOException {
+        ContractParameter key = string(KEY2_STRING);
+        InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
+        assertThat(res.getStack().get(0).getString(), is(DATA2));
+    }
+
+    // getInteger
+
+    @Test
+    public void getIntegerByByteArrayKey() throws IOException {
+        ContractParameter key = byteArray(KEY3_HEX);
+        InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
+        assertThat(res.getStack().get(0).getInteger(), is(INTEGER3));
+    }
+
+    @Test
+    public void getIntegerByByteStringKey() throws IOException {
+        ContractParameter key = byteArray(KEY3_HEX);
+        InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
+        assertThat(res.getStack().get(0).getInteger(), is(INTEGER3));
+    }
+
+    @Test
+    public void getIntegerByStringKey() throws IOException {
+        ContractParameter key = string(KEY4_STRING);
+        InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
+        assertThat(res.getStack().get(0).getInteger(), is(INTEGER4));
     }
 
     @Test
@@ -245,7 +329,7 @@ public class StorageIntegrationTest {
 
     @Test
     public void deleteByByteArrayKey() throws IOException {
-        ContractParameter key = byteArray(KEY1);
+        ContractParameter key = byteArray(KEY1_HEX);
         InvocationResult res = ct.callInvokeFunction("getByByteArrayKey", key)
                 .getInvocationResult();
         assertThat(res.getStack().get(0).getHexString(), is(DATA1));
@@ -255,7 +339,7 @@ public class StorageIntegrationTest {
 
     @Test
     public void deleteByByteStringKey() throws IOException {
-        ContractParameter key = byteArray(KEY1);
+        ContractParameter key = byteArray(KEY1_HEX);
         InvocationResult res = ct.callInvokeFunction("getByByteStringKey", key)
                 .getInvocationResult();
         assertThat(res.getStack().get(0).getHexString(), is(DATA1));
@@ -265,7 +349,7 @@ public class StorageIntegrationTest {
 
     @Test
     public void deleteByStringKey() throws IOException {
-        ContractParameter key = string(KEY2);
+        ContractParameter key = string(KEY2_STRING);
         InvocationResult res = ct.callInvokeFunction("getByStringKey", key).getInvocationResult();
         assertThat(res.getStack().get(0).getString(), is(DATA2));
         res = ct.callInvokeFunction(testName, key).getInvocationResult();
@@ -274,28 +358,28 @@ public class StorageIntegrationTest {
 
     @Test
     public void findByByteStringPrefix() throws IOException {
-        ContractParameter key = byteArray(KEY1);
+        ContractParameter key = byteArray(KEY1_HEX);
         InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
         List<StackItem> entry = res.getStack().get(0).getList();
-        assertThat(entry.get(0).getHexString(), is(KEY1));
+        assertThat(entry.get(0).getHexString(), is(KEY1_HEX));
         assertThat(entry.get(1).getHexString(), is(DATA1));
     }
 
     @Test
     public void findByByteArrayPrefix() throws IOException {
-        ContractParameter key = byteArray(KEY1);
+        ContractParameter key = byteArray(KEY1_HEX);
         InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
         List<StackItem> entry = res.getStack().get(0).getList();
-        assertThat(entry.get(0).getHexString(), is(KEY1));
+        assertThat(entry.get(0).getHexString(), is(KEY1_HEX));
         assertThat(entry.get(1).getHexString(), is(DATA1));
     }
 
     @Test
     public void findByStringPrefix() throws IOException {
-        ContractParameter key = string(KEY2);
+        ContractParameter key = string(KEY2_STRING);
         InvocationResult res = ct.callInvokeFunction(testName, key).getInvocationResult();
         List<StackItem> entry = res.getStack().get(0).getList();
-        assertThat(entry.get(0).getString(), is(KEY2));
+        assertThat(entry.get(0).getString(), is(KEY2_STRING));
         assertThat(entry.get(1).getString(), is(DATA2));
     }
 
@@ -311,7 +395,7 @@ public class StorageIntegrationTest {
     public void findWithFindOptionDeserializeValues() throws IOException {
         InvocationResult res = ct.callInvokeFunction(testName).getInvocationResult();
         Map<StackItem, StackItem> map = res.getStack().get(0).getMap();
-        assertTrue(map.containsKey(new ByteStringStackItem(KEY1)));
+        assertTrue(map.containsKey(new ByteStringStackItem(KEY1_HEX)));
         assertTrue(map.containsKey(new ByteStringStackItem("0102")));
     }
 
@@ -322,6 +406,12 @@ public class StorageIntegrationTest {
         public static void storeData(byte[] key, byte[] data) {
             Storage.put(ctx, key, data);
         }
+
+        public static void storeInteger(byte[] key, int value) {
+            Storage.put(ctx, key, value);
+        }
+
+        // get
 
         public static ByteString getByByteArrayKey(byte[] key) {
             return Storage.get(ctx, key);
@@ -334,6 +424,50 @@ public class StorageIntegrationTest {
         public static ByteString getByStringKey(String key) {
             return Storage.get(ctx, key);
         }
+
+        // getByteArray
+
+        public static byte[] getByteArrayByByteArrayKey(byte[] key) {
+            return Storage.getByteArray(ctx, key);
+        }
+
+        public static byte[] getByteArrayByByteStringKey(ByteString key) {
+            return Storage.getByteArray(ctx, key);
+        }
+
+        public static byte[] getByteArrayByStringKey(String key) {
+            return Storage.getByteArray(ctx, key);
+        }
+
+        // getString
+
+        public static String getStringByByteArrayKey(byte[] key) {
+            return Storage.getString(ctx, key);
+        }
+
+        public static String getStringByByteStringKey(ByteString key) {
+            return Storage.getString(ctx, key);
+        }
+
+        public static String getStringByStringKey(String key) {
+            return Storage.getString(ctx, key);
+        }
+
+        // getInteger
+
+        public static int getIntegerByByteArrayKey(byte[] key) {
+            return Storage.getInteger(ctx, key);
+        }
+
+        public static int getIntegerByByteStringKey(ByteString key) {
+            return Storage.getInteger(ctx, key);
+        }
+
+        public static int getIntegerByStringKey(String key) {
+            return Storage.getInteger(ctx, key);
+        }
+
+        // putByteArray
 
         public static ByteString putByteArrayKeyByteArrayValue(byte[] key, byte[] value) {
             Storage.put(ctx, key, value);
@@ -367,6 +501,8 @@ public class StorageIntegrationTest {
             return Storage.get(ctx, key);
         }
 
+        // putByteString
+
         public static ByteString putByteStringKeyByteArrayValue(ByteString key, byte[] value) {
             Storage.put(ctx, key, value);
             return Storage.get(ctx, key);
@@ -398,6 +534,8 @@ public class StorageIntegrationTest {
             Storage.put(ctx, key, value);
             return Storage.get(ctx, key);
         }
+
+        // putString
 
         public static ByteString putStringKeyByteArrayValue(String key, byte[] value) {
             Storage.put(ctx, key, value);
@@ -431,6 +569,8 @@ public class StorageIntegrationTest {
             return Storage.get(ctx, key);
         }
 
+        // delete
+
         public static ByteString deleteByByteArrayKey(byte[] key) {
             Storage.delete(ctx, key);
             return Storage.get(ctx, key);
@@ -445,6 +585,8 @@ public class StorageIntegrationTest {
             Storage.delete(ctx, key);
             return Storage.get(ctx, key);
         }
+
+        // find
 
         public static Map.Entry<ByteString, ByteString> findByByteStringPrefix(ByteString prefix) {
             Iterator<Map.Entry<ByteString, ByteString>> it = Storage.find(ctx, prefix,
--- devpack/src/main/java/io/neow3j/devpack/Storage.java
+++ devpack/src/main/java/io/neow3j/devpack/Storage.java
@@ -46,6 +46,53 @@ public class Storage {
     public static native ByteString get(StorageContext context, byte[] key);
 
     /**
+     * Returns the value corresponding to the given key and converts it to a byte array.
+     * <p>
+     * This incurs the GAS cost of converting the {@code ByteString} value to a byte array.
+     *
+     * @param context The storage context to search in.
+     * @param key     The key to search for.
+     * @return the value corresponding to the given key converted to a byte array.
+     */
+    @Instruction(opcode = OpCode.CONVERT, operand = StackItemType.BUFFER_CODE)
+    @Syscall(SYSTEM_STORAGE_GET)
+    public static native byte[] getByteArray(StorageContext context, byte[] key);
+
+    /**
+     * Returns the value corresponding to the given key as a string.
+     * <p>
+     * This does not incur any extra GAS costs.
+     *
+     * @param context The storage context to search in.
+     * @param key     The key to search for.
+     * @return the value corresponding to the given key as a string.
+     */
+    @Instruction()
+    @Syscall(SYSTEM_STORAGE_GET)
+    public static native String getString(StorageContext context, byte[] key);
+
+    /**
+     * Returns the value corresponding to the given key and converts it to an integer. The bytes
+     * are read in little-endian format. E.g., the byte string {@code 0102} (in hexadecimal
+     * representation) is converted to 513.
+     * <p>
+     * This incurs the GAS cost of converting the {@code ByteString} value to an integer.
+     *
+     * @param context The storage context to search in.
+     * @param key     The key to search for.
+     * @return the value corresponding to the given key converted to an integer.
+     */
+    @Instruction(opcode = OpCode.DUP)
+    @Instruction(opcode = OpCode.ISNULL)
+    @Instruction(opcode = OpCode.JMPIFNOT, operand = 0x05)
+    @Instruction(opcode = OpCode.PUSH0)
+    @Instruction(opcode = OpCode.SWAP)
+    @Instruction(opcode = OpCode.DROP)
+    @Instruction(opcode = OpCode.CONVERT, operand = StackItemType.INTEGER_CODE)
+    @Syscall(SYSTEM_STORAGE_GET)
+    public static native int getInteger(StorageContext context, byte[] key);
+
+    /**
      * Returns the value corresponding to the given key.
      *
      * @param context The storage context to search in.
@@ -55,6 +102,43 @@ public class Storage {
     @Syscall(SYSTEM_STORAGE_GET)
     public static native ByteString get(StorageContext context, String key);
 
+    /**
+     * Returns the value corresponding to the given key and converts it to a byte array.
+     * <p>
+     * This incurs the GAS cost of converting the {@code ByteString} value to a byte array.
+     *
+     * @param context The storage context to search in.
+     * @param key     The key to search for.
+     * @return the value corresponding to the given key converted to a byte array.
+     */
+    @Instruction(opcode = OpCode.CONVERT, operand = StackItemType.BUFFER_CODE)
+    @Syscall(SYSTEM_STORAGE_GET)
+    public static native byte[] getByteArray(StorageContext context, String key);
+
+    /**
+     * Returns the value corresponding to the given key as a string.
+     * <p>
+     * This does not incur any extra GAS costs.
+     *
+     * @param context The storage context to search in.
+     * @param key     The key to search for.
+     * @return the value corresponding to the given key as a string.
+     */
+    @Instruction()
+    @Syscall(SYSTEM_STORAGE_GET)
+    public static native String getString(StorageContext context, String key);
+
+    /**
+     * Returns the value corresponding to the given key and converts it to an integer. The bytes
+     * are read in little-endian format. E.g., the byte string {@code 0102} (in hexadecimal
+     * representation) is converted to 513.
+     * <p>
+     * This incurs the GAS cost of converting the {@code ByteString} value to an integer.
+     *
+     * @param context The storage context to search in.
+     * @param key     The key to search for.
+     * @return the value corresponding to the given key converted to an integer.
+     */
     @Instruction(opcode = OpCode.DUP)
     @Instruction(opcode = OpCode.ISNULL)
     @Instruction(opcode = OpCode.JMPIFNOT, operand = 0x05)
@@ -62,7 +146,8 @@ public class Storage {
     @Instruction(opcode = OpCode.SWAP)
     @Instruction(opcode = OpCode.DROP)
     @Instruction(opcode = OpCode.CONVERT, operand = StackItemType.INTEGER_CODE)
-    public static native int getInt(StorageContext context, String key);
+    @Syscall(SYSTEM_STORAGE_GET)
+    public static native int getInteger(StorageContext context, String key);
 
     /**
      * Returns the value corresponding to the given key.
@@ -73,6 +158,53 @@ public class Storage {
      */
     @Syscall(SYSTEM_STORAGE_GET)
     public static native ByteString get(StorageContext context, ByteString key);
+
+    /**
+     * Returns the value corresponding to the given key and converts it to a byte array.
+     * <p>
+     * This incurs the GAS cost of converting the {@code ByteString} value to a byte array.
+     *
+     * @param context The storage context to search in.
+     * @param key     The key to search for.
+     * @return the value corresponding to the given key converted to a byte array.
+     */
+    @Instruction(opcode = OpCode.CONVERT, operand = StackItemType.BUFFER_CODE)
+    @Syscall(SYSTEM_STORAGE_GET)
+    public static native byte[] getByteArray(StorageContext context, ByteString key);
+
+    /**
+     * Returns the value corresponding to the given key as a string.
+     * <p>
+     * This does not incur any extra GAS costs.
+     *
+     * @param context The storage context to search in.
+     * @param key     The key to search for.
+     * @return the value corresponding to the given key as a string.
+     */
+    @Instruction()
+    @Syscall(SYSTEM_STORAGE_GET)
+    public static native String getString(StorageContext context, ByteString key);
+
+    /**
+     * Returns the value corresponding to the given key and converts it to an integer. The bytes
+     * are read in little-endian format. E.g., the byte string {@code 0102} (in hexadecimal
+     * representation) is converted to 513.
+     * <p>
+     * This incurs the GAS cost of converting the {@code ByteString} value to an integer.
+     *
+     * @param context The storage context to search in.
+     * @param key     The key to search for.
+     * @return the value corresponding to the given key converted to an integer.
+     */
+    @Instruction(opcode = OpCode.DUP)
+    @Instruction(opcode = OpCode.ISNULL)
+    @Instruction(opcode = OpCode.JMPIFNOT, operand = 0x05)
+    @Instruction(opcode = OpCode.PUSH0)
+    @Instruction(opcode = OpCode.SWAP)
+    @Instruction(opcode = OpCode.DROP)
+    @Instruction(opcode = OpCode.CONVERT, operand = StackItemType.INTEGER_CODE)
+    @Syscall(SYSTEM_STORAGE_GET)
+    public static native int getInteger(StorageContext context, ByteString key);
 
     /**
      * Stores the given key-value pair.
@@ -114,11 +246,25 @@ public class Storage {
     @Syscall(SYSTEM_STORAGE_PUT)
     public static native void put(StorageContext context, byte[] key, String value);
 
+    /**
+     * Stores the given key-value pair.
+     *
+     * @param context The storage context to store the value in.
+     * @param key     The key.
+     * @param value   The value to store.
+     */
     @Syscall(SYSTEM_STORAGE_PUT)
-    public static native void put(StorageContext context, byte[] key, Hash160 hash160);
+    public static native void put(StorageContext context, byte[] key, Hash160 value);
 
+    /**
+     * Stores the given key-value pair.
+     *
+     * @param context The storage context to store the value in.
+     * @param key     The key.
+     * @param value   The value to store.
+     */
     @Syscall(SYSTEM_STORAGE_PUT)
-    public static native void put(StorageContext context, byte[] key, Hash256 hash256);
+    public static native void put(StorageContext context, byte[] key, Hash256 value);
 
     /**
      * Stores the given key-value pair.
@@ -160,11 +306,25 @@ public class Storage {
     @Syscall(SYSTEM_STORAGE_PUT)
     public static native void put(StorageContext context, ByteString key, ByteString value);
 
+    /**
+     * Stores the given key-value pair.
+     *
+     * @param context The storage context to store the value in.
+     * @param key     The key.
+     * @param value   The value to store.
+     */
     @Syscall(SYSTEM_STORAGE_PUT)
-    public static native void put(StorageContext context, ByteString key, Hash160 hash160);
+    public static native void put(StorageContext context, ByteString key, Hash160 value);
 
+    /**
+     * Stores the given key-value pair.
+     *
+     * @param context The storage context to store the value in.
+     * @param key     The key.
+     * @param value   The value to store.
+     */
     @Syscall(SYSTEM_STORAGE_PUT)
-    public static native void put(StorageContext context, ByteString key, Hash256 hash256);
+    public static native void put(StorageContext context, ByteString key, Hash256 value);
 
     /**
      * Stores the given key-value pair.
@@ -206,11 +366,25 @@ public class Storage {
     @Syscall(SYSTEM_STORAGE_PUT)
     public static native void put(StorageContext context, String key, ByteString value);
 
+    /**
+     * Stores the given key-value pair.
+     *
+     * @param context The storage context to store the value in.
+     * @param key     The key.
+     * @param value   The value to store.
+     */
     @Syscall(SYSTEM_STORAGE_PUT)
-    public static native void put(StorageContext context, String key, Hash160 hash160);
+    public static native void put(StorageContext context, String key, Hash160 value);
 
+    /**
+     * Stores the given key-value pair.
+     *
+     * @param context The storage context to store the value in.
+     * @param key     The key.
+     * @param value   The value to store.
+     */
     @Syscall(SYSTEM_STORAGE_PUT)
-    public static native void put(StorageContext context, String key, Hash256 hash256);
+    public static native void put(StorageContext context, String key, Hash256 value);
 
     /**
      * Deletes the value corresponding to the given key from the storage.
