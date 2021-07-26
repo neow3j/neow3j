661b97e2258d0fd6a290abbe3b4731c945f9b352
Michael Bucher <michael@axlabs.com>
2021-07-26 12:19:09 +0200

    #606: Add initial integration test setup using a neo-express node

---
+++ core/src/test-integration/java/io/neow3j/protocol/Neow3jExpressIntegrationTest.java
@@ -0,0 +1,44 @@
+package io.neow3j.protocol;
+
+import io.neow3j.protocol.core.Neow3jExpress;
+import io.neow3j.protocol.core.response.PopulatedBlocks;
+import io.neow3j.protocol.http.HttpService;
+import io.neow3j.test.NeoTestContainer;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static io.neow3j.protocol.IntegrationTestHelper.NEO_HASH;
+import static io.neow3j.test.NeoTestContainer.getNodeUrl;
+import static io.neow3j.utils.Await.waitUntilOpenWalletHasBalanceGreaterThanOrEqualTo;
+import static org.junit.Assert.assertNotNull;
+
+public class Neow3jExpressIntegrationTest {
+    protected static Neow3jExpress neow3jExpress;
+
+    @ClassRule
+    public static NeoTestContainer neoTestContainer = new NeoTestContainer(true);
+
+    @BeforeClass
+    public static void setUp() {
+        neow3jExpress = Neow3jExpress.build(new HttpService(getNodeUrl(neoTestContainer)));
+//        waitUntilOpenWalletHasBalanceGreaterThanOrEqualTo("1", NEO_HASH, getNeow3jExpress());
+    }
+
+    private static Neow3jExpress getNeow3jExpress() {
+        return neow3jExpress;
+    }
+
+    @Test
+    public void test() throws IOException {
+        PopulatedBlocks populatedBlocks = getNeow3jExpress()
+                .expressGetPopulatedBlocks()
+                .send()
+                .getPopulatedBlocks();
+
+        assertNotNull(populatedBlocks);
+    }
+
+}
--- test-tools/src/main/java/io/neow3j/test/NeoTestContainer.java
+++ test-tools/src/main/java/io/neow3j/test/NeoTestContainer.java
@@ -6,6 +6,7 @@ import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
+import static io.neow3j.test.TestProperties.neo3ExpressPrivateNetContainerImg;
 import static io.neow3j.test.TestProperties.neo3PrivateNetContainerImg;
 
 public class NeoTestContainer extends GenericContainer<NeoTestContainer> {
@@ -21,6 +22,9 @@ public class NeoTestContainer extends GenericContainer<NeoTestContainer> {
     static final String ORACLECONFIG_FILE_SOURCE = "/node-config/oracle.config.json";
     static final String ORACLECONFIG_FILE_DESTINATION = "/neo-cli/Plugins/OracleService/config"
             + ".json";
+
+    static final String DEFAULT_NEO_EXPRESS_SOURCE = "/default.neo-express";
+    static final String DEFAULT_NEO_EXPRESS_DESTINATION = "/app/default.neo-express";
 
     // This is the port of one of the .NET nodes which is exposed internally by the container.
     static final int EXPOSED_JSONRPC_PORT = 40332;
@@ -50,12 +54,21 @@ public class NeoTestContainer extends GenericContainer<NeoTestContainer> {
         }
     }
 
+    public NeoTestContainer(boolean expressNode) {
+        super(DockerImageName.parse(neo3ExpressPrivateNetContainerImg()));
+        withCopyFileToContainer(MountableFile.forClasspathResource(DEFAULT_NEO_EXPRESS_SOURCE, 777),
+                DEFAULT_NEO_EXPRESS_DESTINATION);
+        withExposedPorts(EXPOSED_JSONRPC_PORT);
+        waitingFor(Wait.forListeningPort());
+    }
+
     public static String getResultFilePath(String testClassName, String methodName) {
         return "responses/" + testClassName + "/" + methodName + ".json";
     }
 
     public static String getNodeUrl(NeoTestContainer container) {
-        return "http://" + container.getContainerIpAddress() +
-                ":" + container.getMappedPort(EXPOSED_JSONRPC_PORT);
+        return "http://" + container.getContainerIpAddress() + ":" +
+                container.getMappedPort(EXPOSED_JSONRPC_PORT);
     }
+
 }
