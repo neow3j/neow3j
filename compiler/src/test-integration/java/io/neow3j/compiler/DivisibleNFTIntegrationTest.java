6cea5c54b645de3b20a1552cf5adb0be08a33c7a
Michael Bucher <michael@axlabs.com>
2021-10-21 12:57:38 +0200

    (minor) suppress unchecked warnings

--- compiler/src/test-integration/java/io/neow3j/compiler/DivisibleNFTIntegrationTest.java
+++ compiler/src/test-integration/java/io/neow3j/compiler/DivisibleNFTIntegrationTest.java
@@ -32,6 +32,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+@SuppressWarnings("unchecked")
 public class DivisibleNFTIntegrationTest {
 
     @Rule
