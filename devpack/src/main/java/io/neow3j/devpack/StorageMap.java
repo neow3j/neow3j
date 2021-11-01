9a313a50d7752d31f1531eb6f9ae4e373f91df16
Michael Bucher <michael@axlabs.com>
2021-11-01 16:56:59 +0100

    #667: Add StorageMap method to use integer keys

--- devpack/src/main/java/io/neow3j/devpack/StorageMap.java
+++ devpack/src/main/java/io/neow3j/devpack/StorageMap.java
@@ -27,6 +27,8 @@ public class StorageMap {
         this.prefix = prefix;
     }
 
+    // region delete
+
     /**
      * Deletes the entry with a key equal to {@code prefix + key} from the underlying storage
      * context.
@@ -56,6 +58,19 @@ public class StorageMap {
     public void delete(String key) {
         Storage.delete(context, concat(prefix, key));
     }
+
+    /**
+     * Deletes the entry with a key equal to {@code prefix + key} from the underlying storage
+     * context.
+     *
+     * @param key The key to delete.
+     */
+    public void delete(int key) {
+        Storage.delete(context, concat(prefix, key));
+    }
+
+    // endregion delete
+    // region get bytearray key
 
     /**
      * Gets the value with a key equal to {@code prefix + key} from the underlying storage context.
@@ -103,6 +118,9 @@ public class StorageMap {
         return Storage.getInteger(context, concat(prefix, key));
     }
 
+    // endregion get bytearray key
+    // region get bytestring key
+
     /**
      * Gets the value with a key equal to {@code prefix + key} from the underlying storage context.
      *
@@ -149,6 +167,9 @@ public class StorageMap {
         return Storage.getInteger(context, concat(prefix, key));
     }
 
+    // endregion get bytestring key
+    // region get string key
+
     /**
      * Gets the value with a key equal to {@code prefix + key} from the underlying storage context.
      *
@@ -194,6 +215,58 @@ public class StorageMap {
     public Integer getInteger(String key) {
         return Storage.getInteger(context, concat(prefix, key));
     }
+
+    // endregion get string key
+    // region get integer key
+
+    /**
+     * Gets the value with a key equal to {@code prefix + key} from the underlying storage context.
+     *
+     * @param key The key of the value to retrieve.
+     * @return the value corresponding to the given key.
+     */
+    public ByteString get(int key) {
+        return Storage.get(context, concat(prefix, key));
+    }
+
+    /**
+     * Gets the value with a key equal to {@code prefix + key} from the underlying storage context
+     * and converts it to a byte array.
+     *
+     * @param key The key of the value to retrieve.
+     * @return the value corresponding to the given key converted to a byte array.
+     */
+    public byte[] getByteArray(int key) {
+        return Storage.getByteArray(context, concat(prefix, key));
+    }
+
+    /**
+     * Gets the value with a key equal to {@code prefix + key} from the underlying storage context
+     * as a string.
+     *
+     * @param key The key of the value to retrieve.
+     * @return the value corresponding to the given key as a string.
+     */
+    public String getString(int key) {
+        return Storage.getString(context, concat(prefix, key));
+    }
+
+    /**
+     * Gets the value with a key equal to {@code prefix + key} from the underlying storage context
+     * and converts it to an integer. The bytes are read in little-endian format. E.g., the byte
+     * string {@code 0102} (in hexadecimal representation) is converted to 513.
+     * <p>
+     * This incurs the GAS cost of converting the {@code ByteString} value to an integer.
+     *
+     * @param key The key of the value to retrieve.
+     * @return the value corresponding to the given key converted to an integer.
+     */
+    public Integer getInteger(int key) {
+        return Storage.getInteger(context, concat(prefix, key));
+    }
+
+    // endregion get integer key
+    // region put bytearray key
 
     /**
      * Stores the given key-value pair prefixed with this {@code StorageMap}'s prefix ({@code
@@ -260,6 +333,9 @@ public class StorageMap {
     public void put(byte[] key, Hash256 value) {
         Storage.put(context, concat(prefix, key), value);
     }
+
+    // endregion put bytearray key
+    // region put bytestring key
 
     /**
      * Stores the given key-value pair prefixed with this {@code StorageMap}'s prefix ({@code
@@ -328,6 +404,9 @@ public class StorageMap {
         Storage.put(context, concat(prefix, key), value);
     }
 
+    // endregion put bytestring key
+    // region put string key
+
     /**
      * Stores the given key-value pair prefixed with this {@code StorageMap}'s prefix ({@code
      * prefix + key}) into the underlying storage context.
@@ -395,6 +474,35 @@ public class StorageMap {
         Storage.put(context, concat(prefix, key), value);
     }
 
+    // endregion put string key
+    // region put integer key
+
+    public void put(int key, byte[] value) {
+        Storage.put(context, concat(prefix, key), value);
+    }
+
+    public void put(int key, int value) {
+        Storage.put(context, concat(prefix, key), value);
+    }
+
+    public void put(int key, ByteString value) {
+        Storage.put(context, concat(prefix, key), value);
+    }
+
+    public void put(int key, String value) {
+        Storage.put(context, concat(prefix, key), value);
+    }
+
+    public void put(int key, Hash160 value) {
+        Storage.put(context, concat(prefix, key), value);
+    }
+
+    public void put(int key, Hash256 value) {
+        Storage.put(context, concat(prefix, key), value);
+    }
+
+    // endregion put integer key
+
     /**
      * Compares this {@code StorageMap} to the given object. The comparison happens by reference
      * only.
@@ -418,7 +526,7 @@ public class StorageMap {
             return true;
         }
         return context.equals(map.context)
-                // We convert the prefix to byte strings for comparison because the neo-vm
+                // The prefix is converted to a byte string for comparison because the neo-vm
                 // does not compare Buffers (which byte[] is on the neo-vm) by value.
                 && new ByteString(prefix).equals(new ByteString(map.prefix));
     }
