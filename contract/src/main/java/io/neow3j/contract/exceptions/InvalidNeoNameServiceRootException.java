2389d9ae85b277026ed0a022061970919bfdc31a
Michael Bucher <michael@axlabs.com>
2022-10-28 10:47:21 +0200

    (minor) Add javadoc to InvalidNeoNameServiceRootException

--- contract/src/main/java/io/neow3j/contract/exceptions/InvalidNeoNameServiceRootException.java
+++ contract/src/main/java/io/neow3j/contract/exceptions/InvalidNeoNameServiceRootException.java
@@ -2,6 +2,9 @@ package io.neow3j.contract.exceptions;
 
 import static java.lang.String.format;
 
+/**
+ * Is thrown when instantiating a {@link io.neow3j.contract.types.NNSName.NNSRoot} with an invalid root.
+ */
 public class InvalidNeoNameServiceRootException extends Exception {
 
     public InvalidNeoNameServiceRootException(String invalidRoot) {
