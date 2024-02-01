437ffc9dbfdcb473637d70d68b168beccd78d075
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-02-01 08:22:13 -0300

    add getBuildDirectory property for compile task

--- gradle-plugin/src/main/java/io/neow3j/devpack/gradle/Neow3jCompileTask.java
+++ gradle-plugin/src/main/java/io/neow3j/devpack/gradle/Neow3jCompileTask.java
@@ -6,6 +6,7 @@ import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.options.Option;
 
@@ -13,6 +14,9 @@ abstract public class Neow3jCompileTask extends DefaultTask {
     static final String CLASSNAME_NAME = "className";
     private static final String DEBUG_NAME = "debug";
     private static final String OUTPUT_DIR_NAME = "outputDir";
+
+    @InputDirectory
+    public abstract DirectoryProperty getBuildDirectory();
 
     @Input
     @Option(option = CLASSNAME_NAME, description = "Sets the smart contract class name (fully qualified name) to be " +
