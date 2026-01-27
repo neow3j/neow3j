80ccd2929770e6eecc4ed5d3b10f051be6dc5858
Michael Bucher <michael@axlabs.com>
2026-01-27 11:42:18 +0100

    chore: use 1s blocktime without waiting for another block

--- int-tests/src/test-integration/java/io/neow3j/test/ModuleTest.java
+++ int-tests/src/test-integration/java/io/neow3j/test/ModuleTest.java
@@ -194,7 +194,6 @@ public class ModuleTest {
     @Test
     @Order(5)
     public void fastForwardMultipleBlocksWithTimeUnits() throws Throwable {
-        Await.waitUntilBlockCountIsGreaterThan(neow3j, currentBlockIndex(neow3j).add(BigInteger.ONE));
         // Forward blocks and time with distinct time units
         BigInteger startIndex = currentBlockIndex(neow3j);
         long startTime = getBlockTimeInSeconds(neow3j, startIndex);
