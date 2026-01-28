eca080933852879f13bc7e7ea7ade7c6edc53da5
Michael Bucher <michael@axlabs.com>
2026-01-28 19:30:30 +0100

    chore: mark StorageContext as deprecated (#1134)

--- devpack/src/main/java/io/neow3j/devpack/StorageContext.java
+++ devpack/src/main/java/io/neow3j/devpack/StorageContext.java
@@ -9,7 +9,10 @@ import static io.neow3j.devpack.constants.InteropService.SYSTEM_STORAGE_ASREADON
  * A {@code StorageContext} is the gateway to a contract's storage. It can be passed to other contracts as an
  * argument, allowing them to perform read/write operations on the persistent store of the current contract. It is
  * required in all {@link Storage} methods.
+ *
+ * @deprecated since 3.24.1; planned to be removed in 3.25.0.
  */
+@Deprecated
 public class StorageContext implements InteropInterface {
 
     private StorageContext() {
@@ -20,7 +23,9 @@ public class StorageContext implements InteropInterface {
      * the contract's storage is denied.
      *
      * @return this {@code StorageContext}.
+     * @deprecated since 3.24.1; planned to be removed in 3.25.0.
      */
+    @Deprecated
     @Instruction(interopService = SYSTEM_STORAGE_ASREADONLY)
     public native StorageContext asReadOnly();
 
@@ -29,7 +34,9 @@ public class StorageContext implements InteropInterface {
      *
      * @param other the object to compare with.
      * @return true if this and {@code other} reference the same storage context. False, otherwise.
+     * @deprecated since 3.24.1; planned to be removed in 3.25.0.
      */
+    @Deprecated
     @Override
     @Instruction(opcode = OpCode.EQUAL)
     public native boolean equals(Object other);
