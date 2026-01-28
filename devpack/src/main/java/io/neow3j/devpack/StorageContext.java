252a7357b1913c0711e6c28fe7bb08ba3511227f
Michael Bucher <michael@axlabs.com>
2026-01-28 19:42:29 +0100

    chore: update StorageContext's class javadoc

--- devpack/src/main/java/io/neow3j/devpack/StorageContext.java
+++ devpack/src/main/java/io/neow3j/devpack/StorageContext.java
@@ -6,11 +6,21 @@ import io.neow3j.devpack.constants.OpCode;
 import static io.neow3j.devpack.constants.InteropService.SYSTEM_STORAGE_ASREADONLY;
 
 /**
- * A {@code StorageContext} is the gateway to a contract's storage. It can be passed to other contracts as an
- * argument, allowing them to perform read/write operations on the persistent store of the current contract. It is
- * required in all {@link Storage} methods.
+ * A {@code StorageContext} represents access to a contract’s persistent storage. It can be passed to other contracts
+ * as an argument, allowing them to perform read and write operations on the storage of the originating contract.
+ * <p>
+ * Traditionally, a {@code StorageContext} was required by all {@link Storage} methods to determine which contract’s
+ * storage should be accessed.
  *
  * @deprecated since 3.24.1; planned to be removed in 3.25.0.
+ * <p>
+ * Since the Faun hard fork, new storage-related system calls operate on the storage of the executing contract
+ * without requiring an explicit {@code StorageContext}. For accessing a contract’s own storage, providing a context
+ * is therefore no longer necessary.
+ * <p>
+ * Context-based storage system calls remain available for backwards compatibility and advanced use cases (such as
+ * explicitly delegating storage access to another contract). However, their use is discouraged in new code, as it is
+ * easier to misuse and can unintentionally broaden storage write access.
  */
 @Deprecated
 public class StorageContext implements InteropInterface {
